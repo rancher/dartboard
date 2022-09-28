ff0db6b305ac0ccb7ffddd336ff4cf94182ae4d1
Silvio Moioli <silvio@moioli.net>
2022-09-28 09:24:43 +0200

    TF format
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- aws_host/outputs.tf
+++ aws_host/outputs.tf
@@ -2,7 +2,7 @@ resource "local_file" "login_script" {
   content = <<-EOT
     #!/bin/sh
     ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" \
-      %{ if var.ssh_bastion_host != null }-o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${var.ssh_bastion_host}"%{ endif }\
+      %{if var.ssh_bastion_host != null}-o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${var.ssh_bastion_host}"%{endif}\
       root@${aws_instance.instance.private_dns} $@
   EOT
 
