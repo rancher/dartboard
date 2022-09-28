a52fd1878a31cc8173ba2f3574171c7a2741d4db
Silvio Moioli <silvio@moioli.net>
2022-09-28 09:09:44 +0200

    Refactoring: move output to module
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,3 @@
-resource "local_file" "login_bastion" {
-  content = <<-EOT
-    #!/bin/sh
-    ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" root@${module.bastion.public_name} $@
-  EOT
-
-  filename = "${path.module}/config/login_bastion.sh"
-}
-
-resource "local_file" "login_node" {
-  for_each = {
-    for i, name in module.nodes.private_names : i => name
-  }
-
-  filename = "${path.module}/config/login_node-${each.key + 1}.sh"
-
-  content = <<-EOT
-    #!/bin/sh
-    ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -J root@${module.bastion.public_name} root@${each.value} $@
-  EOT
-}
-
-locals {
-  logins = [for i in range(length(module.nodes.private_names)) : "./config/login_node-${i + 1}.sh"]
-}
-
 output "rancher_help" {
   value = <<-EOT
 
