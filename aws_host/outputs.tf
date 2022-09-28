9b2928baf51a1b6f2759de7886dc09e689b9af18
Silvio Moioli <silvio@moioli.net>
2022-09-28 14:11:59 +0200

    Bugfix: unbreak bastion ssh script
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- aws_host/outputs.tf
+++ aws_host/outputs.tf
@@ -2,8 +2,12 @@ resource "local_file" "login_script" {
   content = <<-EOT
     #!/bin/sh
     ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" \
-      %{if var.ssh_bastion_host != null}-o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${var.ssh_bastion_host}"%{endif}\
-      root@${aws_instance.instance.private_dns} $@
+      %{if var.ssh_bastion_host != null~}
+      -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${var.ssh_bastion_host}" root@${aws_instance.instance.private_dns} \
+      %{else~}
+      root@${aws_instance.instance.public_dns} \
+      %{endif~}
+      $@
   EOT
 
   filename = "${path.module}/../config/ssh-to-${aws_instance.instance.private_dns}-${var.name}.sh"
