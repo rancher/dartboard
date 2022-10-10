7ab3b55cf3f8afadd1bc676f338d77da57e8e5fa
Silvio Moioli <silvio@moioli.net>
2022-10-10 16:31:19 +0200

    Refactoring: add higher-level module for AWS RKE2 cluster
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ aws_rke2/main.tf
@@ -0,0 +1,53 @@
+module "server_nodes" {
+  count                 = var.server_count
+  source                = "../aws_host"
+  ami                   = var.ami
+  instance_type         = var.instance_type
+  availability_zone     = var.availability_zone
+  project_name          = var.project_name
+  name                  = "${var.name}-server-node-${count.index}"
+  ssh_key_name          = var.ssh_key_name
+  ssh_private_key_path  = var.ssh_private_key_path
+  subnet_id             = var.subnet_id
+  vpc_security_group_id = var.vpc_security_group_id
+  ssh_bastion_host      = var.ssh_bastion_host
+  ssh_tunnels           = count.index == 0 ? concat([[var.k8s_api_ssh_tunnel_local_port, 6443]], var.additional_ssh_tunnels) : []
+}
+
+module "agent_nodes" {
+  count                 = var.agent_count
+  source                = "../aws_host"
+  ami                   = var.ami
+  instance_type         = var.instance_type
+  availability_zone     = var.availability_zone
+  project_name          = var.project_name
+  name                  = "${var.name}-agent-node-${count.index}"
+  ssh_key_name          = var.ssh_key_name
+  ssh_private_key_path  = var.ssh_private_key_path
+  subnet_id             = var.subnet_id
+  vpc_security_group_id = var.vpc_security_group_id
+  ssh_bastion_host      = var.ssh_bastion_host
+}
+
+module "rke2" {
+  source       = "../rke2"
+  project      = var.project_name
+  name         = var.name
+  server_names = [for node in module.server_nodes : node.private_name]
+  agent_names  = [for node in module.agent_nodes : node.private_name]
+  sans         = var.sans
+
+  ssh_private_key_path = var.ssh_private_key_path
+  ssh_bastion_host     = var.ssh_bastion_host
+  ssh_local_port       = var.k8s_api_ssh_tunnel_local_port
+
+  rke2_version        = var.rke2_version
+  max_pods            = var.max_pods
+  node_cidr_mask_size = var.node_cidr_mask_size
+
+  secret_values = var.secret_values
+}
+
+output "first_server_private_name" {
+  value = module.server_nodes[0].private_name
+}
