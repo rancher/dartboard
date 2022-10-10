c898c866fbf473c038a6908b2ca0d6579d13a02c
Silvio Moioli <silvio@moioli.net>
2022-10-10 16:31:19 +0200

    Refactoring: add higher-level module for AWS RKE2 cluster
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- main.tf
+++ main.tf
@@ -53,56 +53,27 @@ module "bastion" {
   vpc_security_group_id = module.aws_network.public_security_group_id
 }
 
-module "upstream_server_nodes" {
-  depends_on            = [module.aws_network]
-  count                 = local.upstream_server_count
-  source                = "./aws_host"
-  ami                   = local.upstream_ami
-  instance_type         = local.upstream_instance_type
-  availability_zone     = local.availability_zone
-  project_name          = local.project_name
-  name                  = "upstream-server-node-${count.index}"
-  ssh_key_name          = module.aws_shared.key_name
-  ssh_private_key_path  = local.ssh_private_key_path
-  subnet_id             = module.aws_network.private_subnet_id
-  vpc_security_group_id = module.aws_network.private_security_group_id
-  ssh_bastion_host      = module.bastion.public_name
-  ssh_tunnels           = count.index == 0 ? [[local.upstream_local_port, 6443], [3000, 443]] : []
-}
-
-module "upstream_agent_nodes" {
-  depends_on            = [module.aws_network]
-  count                 = local.upstream_agent_count
-  source                = "./aws_host"
-  ami                   = local.upstream_ami
-  instance_type         = local.upstream_instance_type
-  availability_zone     = local.availability_zone
-  project_name          = local.project_name
-  name                  = "upstream-agent-node-${count.index}"
-  ssh_key_name          = module.aws_shared.key_name
-  ssh_private_key_path  = local.ssh_private_key_path
-  subnet_id             = module.aws_network.private_subnet_id
-  vpc_security_group_id = module.aws_network.private_security_group_id
-  ssh_bastion_host      = module.bastion.public_name
-}
-
-module "upstream_rke2" {
-  source       = "./rke2"
-  project      = local.project_name
-  name         = "upstream"
-  server_names = [for node in module.upstream_server_nodes : node.private_name]
-  agent_names  = [for node in module.upstream_agent_nodes : node.private_name]
-  sans         = [local.upstream_san]
-
-  ssh_private_key_path = local.ssh_private_key_path
-  ssh_bastion_host     = module.bastion.public_name
-  ssh_local_port       = local.upstream_local_port
-
-  rke2_version        = local.upstream_rke2_version
-  max_pods            = local.upstream_max_pods
-  node_cidr_mask_size = local.upstream_node_cidr_mask_size
-
-  secret_values = module.secret.values
+module "upstream_cluster" {
+  source                        = "./aws_rke2"
+  ami                           = local.upstream_ami
+  instance_type                 = local.upstream_instance_type
+  availability_zone             = local.availability_zone
+  project_name                  = local.project_name
+  name                          = "upstream"
+  server_count                  = local.upstream_server_count
+  agent_count                   = local.upstream_agent_count
+  ssh_key_name                  = module.aws_shared.key_name
+  ssh_private_key_path          = local.ssh_private_key_path
+  ssh_bastion_host              = module.bastion.public_name
+  subnet_id                     = module.aws_network.private_subnet_id
+  vpc_security_group_id         = module.aws_network.private_security_group_id
+  k8s_api_ssh_tunnel_local_port = 6443
+  additional_ssh_tunnels        = [[3000, 443]]
+  rke2_version                  = local.upstream_rke2_version
+  max_pods                      = local.upstream_max_pods
+  node_cidr_mask_size           = local.upstream_node_cidr_mask_size
+  sans                          = [local.upstream_san]
+  secret_values                 = module.secret.values
 }
 
 provider "helm" {
@@ -115,63 +86,34 @@ provider "helm" {
 }
 
 module "rancher" {
-  depends_on       = [module.upstream_rke2, module.upstream_server_nodes]
+  depends_on       = [module.upstream_cluster]
   count            = local.upstream_server_count > 0 ? 1 : 0
   source           = "./rancher"
   public_name      = local.upstream_san
-  private_name     = module.upstream_server_nodes[0].private_name
+  private_name     = module.upstream_cluster.first_server_private_name
   api_token_string = module.secret.values.api_token_string
   chart            = local.rancher_chart
 }
 
-module "downstream_server_nodes" {
-  depends_on            = [module.aws_network]
-  count                 = local.downstream_server_count
-  source                = "./aws_host"
-  ami                   = local.downstream_ami
-  instance_type         = local.downstream_instance_type
-  availability_zone     = local.availability_zone
-  project_name          = local.project_name
-  name                  = "downstream-server-node-${count.index}"
-  ssh_key_name          = module.aws_shared.key_name
-  ssh_private_key_path  = local.ssh_private_key_path
-  subnet_id             = module.aws_network.private_subnet_id
-  vpc_security_group_id = module.aws_network.private_security_group_id
-  ssh_bastion_host      = module.bastion.public_name
-  ssh_tunnels           = count.index == 0 ? [[local.downstream_local_port, 6443]] : []
-}
-
-module "downstream_agent_nodes" {
-  depends_on            = [module.aws_network]
-  count                 = local.downstream_agent_count
-  source                = "./aws_host"
-  ami                   = local.downstream_ami
-  instance_type         = local.downstream_instance_type
-  availability_zone     = local.availability_zone
-  project_name          = local.project_name
-  name                  = "downstream-agent-node-${count.index}"
-  ssh_key_name          = module.aws_shared.key_name
-  ssh_private_key_path  = local.ssh_private_key_path
-  subnet_id             = module.aws_network.private_subnet_id
-  vpc_security_group_id = module.aws_network.private_security_group_id
-  ssh_bastion_host      = module.bastion.public_name
-}
-
-module "downstream_rke2" {
-  source       = "./rke2"
-  project      = local.project_name
-  name         = "downstream"
-  server_names = [for node in module.downstream_server_nodes : node.private_name]
-  agent_names  = [for node in module.downstream_agent_nodes : node.private_name]
-  sans         = [local.downstream_san]
-
-  ssh_private_key_path = local.ssh_private_key_path
-  ssh_bastion_host     = module.bastion.public_name
-  ssh_local_port       = local.downstream_local_port
-
-  rke2_version        = local.downstream_rke2_version
-  max_pods            = local.downstream_max_pods
-  node_cidr_mask_size = local.downstream_node_cidr_mask_size
-
-  secret_values = module.secret.values
+module "downstream_cluster" {
+  source                        = "./aws_rke2"
+  count                         = 1
+  ami                           = local.downstream_ami
+  instance_type                 = local.downstream_instance_type
+  availability_zone             = local.availability_zone
+  project_name                  = local.project_name
+  name                          = "downstream-${count.index}"
+  server_count                  = local.downstream_server_count
+  agent_count                   = local.downstream_agent_count
+  ssh_key_name                  = module.aws_shared.key_name
+  ssh_private_key_path          = local.ssh_private_key_path
+  ssh_bastion_host              = module.bastion.public_name
+  subnet_id                     = module.aws_network.private_subnet_id
+  vpc_security_group_id         = module.aws_network.private_security_group_id
+  k8s_api_ssh_tunnel_local_port = 6444 + count.index
+  rke2_version                  = local.downstream_rke2_version
+  max_pods                      = local.downstream_max_pods
+  node_cidr_mask_size           = local.downstream_node_cidr_mask_size
+  sans                          = [local.downstream_san]
+  secret_values                 = module.secret.values
 }
