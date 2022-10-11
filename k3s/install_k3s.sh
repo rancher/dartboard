ce75e0ecdd8b9fb3881ad6bafa3fb6cf9be485c8
Silvio Moioli <silvio@moioli.net>
2022-10-11 10:18:25 +0200

    Re-introduce k3s
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ k3s/install_k3s.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# HACK: work around https://github.com/k3s-io/k3s/issues/2306
+sleep ${sleep_time}
+
+# pre-shared secrets
+mkdir -p /var/lib/rancher/k3s/server/tls/
+cat >/var/lib/rancher/k3s/server/tls/client-ca.key <<EOF
+${client_ca_key}
+EOF
+cat >/var/lib/rancher/k3s/server/tls/client-ca.crt <<EOF
+${client_ca_cert}
+EOF
+cat >/var/lib/rancher/k3s/server/tls/server-ca.key <<EOF
+${server_ca_key}
+EOF
+cat >/var/lib/rancher/k3s/server/tls/server-ca.crt <<EOF
+${server_ca_cert}
+EOF
+cat >/var/lib/rancher/k3s/server/tls/request-header-ca.key <<EOF
+${request_header_ca_key}
+EOF
+cat >/var/lib/rancher/k3s/server/tls/request-header-ca.crt <<EOF
+${request_header_ca_cert}
+EOF
+
+mkdir -p /etc/rancher/k3s/
+cat >/etc/rancher/k3s/config.yaml <<EOF
+server: ${jsonencode(server_url)}
+token: ${jsonencode(token)}
+%{ if cluster_init ~}
+cluster-init: true
+%{ endif ~}
+%{ if exec == "server" ~}
+tls-san:
+%{ for san in sans ~}
+  - ${jsonencode(san)}
+%{ endfor ~}
+kube-controller-manager-arg: "node-cidr-mask-size=${node_cidr_mask_size}"
+%{ endif ~}
+kubelet-arg: "config=/etc/rancher/k3s/kubelet-custom.config"
+EOF
+
+cat > /etc/rancher/k3s/kubelet-custom.config <<EOF
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+maxPods: ${max_pods}
+EOF
+
+# installation
+export INSTALL_K3S_VERSION=${k3s_version}
+export INSTALL_K3S_EXEC=${exec}
+
+curl -sfL https://get.k3s.io | sh -
---
+++ k3s/variables.tf
@@ -0,0 +1,72 @@
+variable "project" {
+  description = "A prefix for names of objects created by this module"
+  default     = "st"
+}
+
+variable "k3s_version" {
+  description = "k3s version"
+  default     = "v1.23.10+k3s1"
+}
+
+variable "name" {
+  description = "Symbolic name of this cluster"
+  type        = string
+}
+
+variable "server_names" {
+  description = "List of names of server nodes to deploy"
+  type        = list(string)
+}
+
+variable "agent_names" {
+  description = "List of names of agent nodes to deploy"
+  type        = list(string)
+  default     = []
+}
+
+variable "sans" {
+  description = "Additional Subject Alternative Names"
+  type        = list(string)
+  default     = []
+}
+
+variable "ssh_private_key_path" {
+  description = "Path of private ssh key used to access the instance"
+  type        = string
+}
+
+variable "ssh_bastion_host" {
+  description = "Public name of the SSH bastion host. Leave null for publicly accessible instances"
+  default     = null
+}
+
+variable "ssh_local_port" {
+  description = "Local port for the SSH tunnel for the API"
+  default     = null
+}
+
+variable "max_pods" {
+  description = "Maximum number of pods per node"
+  default     = 110
+}
+
+variable "node_cidr_mask_size" {
+  description = "Size of the CIDR mask for nodes. Increase when increasing max_pods so that 2^(32-node_cidr_max_size) > 2 * max_pods"
+  default     = 24
+}
+
+variable "secret_values" {
+  description = "Value from the secrets module"
+  type = object({
+    client_ca_key          = string
+    client_ca_cert         = string
+    server_ca_key          = string
+    server_ca_cert         = string
+    request_header_ca_key  = string
+    request_header_ca_cert = string
+    master_user_cert       = string
+    master_user_key        = string
+    cluster_ca_certificate = string
+    api_token_string       = string
+  })
+}
