798e58662e8316d698025cfc294c43ddd9756080
Silvio Moioli <silvio@moioli.net>
2022-10-11 10:18:25 +0200

    Re-introduce k3s
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ aws_k3s/variables.tf
@@ -0,0 +1,107 @@
+variable "project_name" {
+  description = "A prefix for names of objects created by this module"
+  default     = "st"
+}
+
+variable "name" {
+  description = "Symbolic name of this cluster"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone where the instance is created"
+  type        = string
+}
+
+variable "server_count" {
+  description = "Number of server nodes in this cluster"
+  default     = 1
+}
+
+variable "agent_count" {
+  description = "Number of agent nodes in this cluster"
+  default     = 0
+}
+
+variable "ami" {
+  description = "AMI ID for all nodes in this cluster"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.large"
+}
+
+variable "ssh_key_name" {
+  description = "Name of the SSH key used to access cluster nodes"
+  type        = string
+}
+
+variable "ssh_private_key_path" {
+  description = "Path of private ssh key used to access cluster nodes"
+  type        = string
+}
+
+variable "ssh_bastion_host" {
+  description = "Public name of the SSH bastion host. Leave null for publicly accessible nodes"
+  default     = null
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet to connect to"
+  type        = string
+}
+
+variable "vpc_security_group_id" {
+  description = "ID of the security group to connect to"
+  type        = string
+}
+
+variable "k8s_api_ssh_tunnel_local_port" {
+  description = "Local port for the SSH tunnel to the first server node's Kubernetes API port (6443)"
+  type        = number
+}
+
+variable "additional_ssh_tunnels" {
+  description = "Opens additional SSH tunnels to the first server node"
+  type        = list(list(number))
+  default     = []
+}
+
+variable "sans" {
+  description = "Additional Subject Alternative Names"
+  type        = list(string)
+  default     = []
+}
+
+variable "k3s_version" {
+  description = "k3s version"
+  default     = "v1.23.10+k3s1"
+}
+
+variable "max_pods" {
+  description = "Maximum number of pods per node"
+  default     = 110
+}
+
+variable "node_cidr_mask_size" {
+  description = "Size of the CIDR mask for nodes. Increase when increasing max_pods so that 2^(32-node_cidr_max_size) > 2 * max_pods"
+  default     = 24
+}
+
+variable "secret_values" {
+  description = "Value from the secrets module"
+  type = object({
+    client_ca_key          = string
+    client_ca_cert         = string
+    server_ca_key          = string
+    server_ca_cert         = string
+    request_header_ca_key  = string
+    request_header_ca_cert = string
+    master_user_cert       = string
+    master_user_key        = string
+    cluster_ca_certificate = string
+    api_token_string       = string
+  })
+}
---
+++ k3s/main.tf
@@ -0,0 +1,127 @@
+terraform {
+  required_providers {
+    ssh = {
+      source = "loafoe/ssh"
+    }
+  }
+}
+
+resource "ssh_sensitive_resource" "first_server_installation" {
+  count        = length(var.server_names) > 0 ? 1 : 0
+  host         = var.server_names[0]
+  private_key  = file(var.ssh_private_key_path)
+  user         = "root"
+  bastion_host = var.ssh_bastion_host
+  timeout      = "600s"
+
+  file {
+    content = templatefile("${path.module}/install_k3s.sh", {
+      k3s_version  = var.k3s_version,
+      sans         = concat([var.server_names[0]], var.sans)
+      exec         = "server"
+      token        = null
+      server_url   = null
+      cluster_init = length(var.server_names) > 1
+
+      client_ca_key          = var.secret_values.client_ca_key
+      client_ca_cert         = var.secret_values.client_ca_cert
+      server_ca_key          = var.secret_values.server_ca_key
+      server_ca_cert         = var.secret_values.server_ca_cert
+      request_header_ca_key  = var.secret_values.request_header_ca_key
+      request_header_ca_cert = var.secret_values.request_header_ca_cert
+      sleep_time             = 0
+      max_pods               = var.max_pods
+      node_cidr_mask_size    = var.node_cidr_mask_size
+    })
+    destination = "/root/install_k3s.sh"
+    permissions = "0700"
+  }
+
+  file {
+    content     = file("${path.module}/wait_for_k8s.sh")
+    destination = "/root/wait_for_k8s.sh"
+    permissions = "0700"
+  }
+
+  commands = [
+    "/root/install_k3s.sh > >(tee install_k3s.log) 2> >(tee install_k3s.err >&2)",
+    "/root/wait_for_k8s.sh",
+    "cat /var/lib/rancher/k3s/server/node-token",
+  ]
+}
+
+resource "ssh_resource" "additional_server_installation" {
+  depends_on = [ssh_sensitive_resource.first_server_installation]
+  count      = length(var.server_names) > 0 ? length(var.server_names) - 1 : 0
+
+  host         = var.server_names[count.index + 1]
+  private_key  = file(var.ssh_private_key_path)
+  user         = "root"
+  bastion_host = var.ssh_bastion_host
+  timeout      = "600s"
+
+  file {
+    content = templatefile("${path.module}/install_k3s.sh", {
+      k3s_version  = var.k3s_version,
+      sans         = [var.server_names[count.index + 1]]
+      exec         = "server"
+      token        = ssh_sensitive_resource.first_server_installation[0].result
+      server_url   = "https://${var.server_names[0]}:6443"
+      cluster_init = false
+
+      client_ca_key          = var.secret_values.client_ca_key
+      client_ca_cert         = var.secret_values.client_ca_cert
+      server_ca_key          = var.secret_values.server_ca_key
+      server_ca_cert         = var.secret_values.server_ca_cert
+      request_header_ca_key  = var.secret_values.request_header_ca_key
+      request_header_ca_cert = var.secret_values.request_header_ca_cert
+      sleep_time             = count.index * 60
+      max_pods               = var.max_pods
+      node_cidr_mask_size    = var.node_cidr_mask_size
+    })
+    destination = "/root/install_k3s.sh"
+    permissions = "0700"
+  }
+
+  commands = [
+    "/root/install_k3s.sh > >(tee install_k3s.log) 2> >(tee install_k3s.err >&2)"
+  ]
+}
+
+resource "ssh_resource" "agent_installation" {
+  depends_on = [ssh_sensitive_resource.first_server_installation]
+  count      = length(var.agent_names)
+
+  host         = var.agent_names[count.index]
+  private_key  = file(var.ssh_private_key_path)
+  user         = "root"
+  bastion_host = var.ssh_bastion_host
+  timeout      = "600s"
+
+  file {
+    content = templatefile("${path.module}/install_k3s.sh", {
+      k3s_version  = var.k3s_version,
+      sans         = [var.agent_names[count.index]]
+      exec         = "agent"
+      token        = ssh_sensitive_resource.first_server_installation[0].result
+      server_url   = "https://${var.server_names[0]}:6443"
+      cluster_init = false
+
+      client_ca_key          = var.secret_values.client_ca_key
+      client_ca_cert         = var.secret_values.client_ca_cert
+      server_ca_key          = var.secret_values.server_ca_key
+      server_ca_cert         = var.secret_values.server_ca_cert
+      request_header_ca_key  = var.secret_values.request_header_ca_key
+      request_header_ca_cert = var.secret_values.request_header_ca_cert
+      sleep_time             = 0
+      max_pods               = var.max_pods
+      node_cidr_mask_size    = var.node_cidr_mask_size
+    })
+    destination = "/root/install_k3s.sh"
+    permissions = "0700"
+  }
+
+  commands = [
+    "/root/install_k3s.sh > >(tee install_k3s.log) 2> >(tee install_k3s.err >&2)"
+  ]
+}
