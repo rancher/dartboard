d2b8ad210748bdcdaf35cd6f5c992e81ecb4bfd5
Silvio Moioli <silvio@moioli.net>
2022-10-12 16:37:24 +0200

    Cypress: allow to import multiple clusters, disable other tests for now
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- cypress/cypress/e2e/imported-clusters.cy.js
+++ cypress/cypress/e2e/imported-clusters.cy.js
@@ -6,55 +6,56 @@ describe('Rancher cluster import functionality', () => {
   })
 
   it('deletes any previously created clusters', () => {
-    cy.visit("/")
-    cy.waitTableLoaded()
-    cy.get('.menu-icon').click()
-    cy.contains('Cluster Management').click()
-    cy.waitTableLoaded()
+    cy.downstreamClusters(name => {
+      cy.visit("/")
+      cy.waitTableLoaded()
+      cy.get('.menu-icon').click()
+      cy.contains('Cluster Management').click()
+      cy.waitTableLoaded()
 
-    cy.get("table").then($table => {
-      if ($table.text().includes('test-cluster')) {
-        cy.get("tr:contains('test-cluster') [role=checkbox]").click()
-        cy.contains("Delete").click()
-        cy.get("[role=dialog] button:contains('Delete')").click()
+      cy.get("table").then($table => {
+        if ($table.text().includes(name)) {
+          cy.get(`tr:contains('${name}') [role=checkbox]`).click()
+          cy.contains("Delete").click()
+          cy.get("[role=dialog] button:contains('Delete')").click()
 
-        cy.contains('test-cluster').should('not.exist')
+          cy.contains('${cluster.name}').should('not.exist')
 
-        // HACK: deleting a cluster results in background operations lasting several seconds
-        // if a new cluster is created in the meantime, creation will fail
-        cy.wait(60000)
-      }})
-  })
-
-  it('creates an imported cluster', () => {
-    // HACK: not going back to the home page results in a Javascript error
-    cy.visit("/")
-    cy.waitTableLoaded()
-    cy.get('.menu-icon').click()
-    cy.contains('Cluster Management').click()
-    cy.waitTableLoaded()
-
-    cy.contains('Import Existing').click()
-    cy.contains('Generic').click()
-    cy.contains('label', 'Cluster Name').next('input').type('test-cluster')
-    cy.contains('Create').click()
-    
-    cy.contains('curl --insecure').should('exist')
-  })
-
-  it('imports a cluster', () => {
-    cy.contains("curl --insecure").then($e => {
-      const registration_command = $e.text()
-      cy.exec(`../config/ssh-*-downstream-server-node-0.sh '${registration_command}'`).then((result) => {
-        cy.log(result.stdout)
+          // HACK: deleting a cluster results in background operations lasting several seconds
+          // if a new cluster is created in the meantime, creation will fail
+          cy.wait(60000)
+        }
       })
     })
+  })
 
-    cy.visit("/")
-    cy.waitTableLoaded()
-    cy.get("tr:contains('test-cluster') .badge-state:contains('Active')", {timeout: 30*60*1000}).should("be.visible")
-    cy.get("tr:contains('test-cluster') .badge-state:contains('Unavailable')", {timeout: 30*60*1000}).should("not.exist")
-    cy.get("tr:contains('test-cluster') .badge-state:contains('Waiting')", {timeout: 30*60*1000}).should("not.exist")
-    cy.get("tr:contains('test-cluster') .col-pods-usage > .icon", {timeout: 30*60*1000}).should("not.exist")
+  it('imports clusters', () => {
+    cy.downstreamClusters((name, first_server_ssh_command) => {
+      // HACK: not going back to the home page results in a Javascript error
+      cy.visit("/")
+      cy.waitTableLoaded()
+      cy.get('.menu-icon').click()
+      cy.contains('Cluster Management').click()
+      cy.waitTableLoaded()
+
+      cy.contains('Import Existing').click()
+      cy.contains('Generic').click()
+      cy.contains('label', 'Cluster Name').next('input').type(name)
+      cy.contains('Create').click()
+
+      cy.contains('curl --insecure').should('exist').then($e => {
+        const registration_command = $e.text()
+        cy.exec(`../config/${first_server_ssh_command} '${registration_command}'`).then((result) => {
+          cy.log(result.stdout)
+
+          cy.visit("/")
+          cy.waitTableLoaded()
+          cy.get(`tr:contains('${name}') .badge-state:contains('Active')`, {timeout: 30 * 60 * 1000}).should("be.visible")
+          cy.get(`tr:contains('${name}') .badge-state:contains('Unavailable')`, {timeout: 30 * 60 * 1000}).should("not.exist")
+          cy.get(`tr:contains('${name}') .badge-state:contains('Waiting')`, {timeout: 30 * 60 * 1000}).should("not.exist")
+          cy.get(`tr:contains('${name}') .col-pods-usage > .icon`, {timeout: 30 * 60 * 1000}).should("not.exist")
+        })
+      })
+    })
   })
 })
