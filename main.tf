f21f00612b470ba0c52ca0e90cd4c694e6dda1fc
Silvio Moioli <silvio@moioli.net>
2022-10-13 14:55:35 +0200

    Change example main.tf: use rke upstream, different downstream clusters
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "bastion" {
 }
 
 module "upstream_cluster" {
-  source                        = "./aws_rke2"
+  source                        = "./aws_rke"
   ami                           = local.upstream_ami
   instance_type                 = local.upstream_instance_type
   availability_zone             = local.availability_zone
@@ -75,23 +75,21 @@ provider "helm" {
 }
 
 module "rancher" {
-  depends_on       = [module.upstream_cluster]
-  count            = local.upstream_server_count > 0 ? 1 : 0
-  source           = "./rancher"
-  public_name      = local.upstream_san
-  private_name     = module.upstream_cluster.first_server_private_name
-  api_token_string = module.secret.values.api_token_string
-  chart            = local.rancher_chart
+  depends_on   = [module.upstream_cluster]
+  count        = local.upstream_server_count > 0 ? 1 : 0
+  source       = "./rancher"
+  public_name  = local.upstream_san
+  private_name = module.upstream_cluster.first_server_private_name
+  chart        = local.rancher_chart
 }
 
-module "downstream_cluster" {
-  source                        = "./aws_rke2"
-  count                         = 1
+module "downstream_cluster_rke" {
+  source                        = "./aws_rke"
   ami                           = local.downstream_ami
   instance_type                 = local.downstream_instance_type
   availability_zone             = local.availability_zone
   project_name                  = local.project_name
-  name                          = "downstream-${count.index}"
+  name                          = "downstream-0"
   server_count                  = local.downstream_server_count
   agent_count                   = local.downstream_agent_count
   ssh_key_name                  = module.aws_shared.key_name
@@ -99,8 +97,47 @@ module "downstream_cluster" {
   ssh_bastion_host              = module.bastion.public_name
   subnet_id                     = module.aws_network.private_subnet_id
   vpc_security_group_id         = module.aws_network.private_security_group_id
-  k8s_api_ssh_tunnel_local_port = 6444 + count.index
-  distro_version                = local.downstream_distro_version
+  k8s_api_ssh_tunnel_local_port = 6444
+  max_pods                      = local.downstream_max_pods
+  node_cidr_mask_size           = local.downstream_node_cidr_mask_size
+  sans                          = [local.downstream_san]
+}
+
+module "downstream_cluster_rke2" {
+  source                        = "./aws_rke2"
+  ami                           = local.downstream_ami
+  instance_type                 = local.downstream_instance_type
+  availability_zone             = local.availability_zone
+  project_name                  = local.project_name
+  name                          = "downstream-1"
+  server_count                  = local.downstream_server_count
+  agent_count                   = local.downstream_agent_count
+  ssh_key_name                  = module.aws_shared.key_name
+  ssh_private_key_path          = local.ssh_private_key_path
+  ssh_bastion_host              = module.bastion.public_name
+  subnet_id                     = module.aws_network.private_subnet_id
+  vpc_security_group_id         = module.aws_network.private_security_group_id
+  k8s_api_ssh_tunnel_local_port = 6445
+  max_pods                      = local.downstream_max_pods
+  node_cidr_mask_size           = local.downstream_node_cidr_mask_size
+  sans                          = [local.downstream_san]
+}
+
+module "downstream_cluster_k3s" {
+  source                        = "./aws_k3s"
+  ami                           = local.downstream_ami
+  instance_type                 = local.downstream_instance_type
+  availability_zone             = local.availability_zone
+  project_name                  = local.project_name
+  name                          = "downstream-2"
+  server_count                  = local.downstream_server_count
+  agent_count                   = local.downstream_agent_count
+  ssh_key_name                  = module.aws_shared.key_name
+  ssh_private_key_path          = local.ssh_private_key_path
+  ssh_bastion_host              = module.bastion.public_name
+  subnet_id                     = module.aws_network.private_subnet_id
+  vpc_security_group_id         = module.aws_network.private_security_group_id
+  k8s_api_ssh_tunnel_local_port = 6446
   max_pods                      = local.downstream_max_pods
   node_cidr_mask_size           = local.downstream_node_cidr_mask_size
   sans                          = [local.downstream_san]
