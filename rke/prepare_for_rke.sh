2b0d68542355e8186ca98c30bc70dfada04a98dd
Silvio Moioli <silvio@moioli.net>
2022-10-13 14:19:22 +0200

    RKE: support Ubuntu as an OS
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- rke/prepare_for_rke.sh
+++ rke/prepare_for_rke.sh
@@ -14,5 +14,17 @@ if grep --quiet --ignore-case suse < /etc/os-release; then
   zypper install -y docker-ce
 fi
 
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+if grep --quiet --ignore-case ubuntu < /etc/os-release; then
+  mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --no-tty --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get install --yes docker-ce docker-ce-cli containerd.io docker-compose-plugin
+fi
+
 systemctl enable docker
 systemctl start docker
