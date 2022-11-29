333030b1f8c3e979905470bf4945da2875cd1f83
Silvio Moioli <silvio@moioli.net>
2022-11-29 15:52:58 +0100

    Add support for RDS databases
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ terraform/aws_rds/variables.tf
@@ -0,0 +1,60 @@
+variable "project_name" {
+  description = "A prefix for names of objects created by this module"
+  default     = "st"
+}
+
+variable "availability_zone" {
+  description = "Availability zone where the instance is created"
+  type        = string
+}
+
+variable "name" {
+  description = "Symbolic name of this instance"
+  type        = string
+}
+
+variable "instance_class" {
+  description = "RDS instance class"
+  default     = "db.t4g.xlarge"
+}
+
+variable "iops" {
+  description = "The amount of provisioned IOPS"
+  default     = null
+  type        = number
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet to connect to"
+  type        = string
+}
+
+variable "secondary_subnet_id" {
+  description = "ID of the secondary subnet to connect to"
+  type        = string
+}
+
+variable "vpc_security_group_id" {
+  description = "ID of the security group to connect to"
+  type        = string
+}
+
+variable "datastore" {
+  description = "Data store to use: mariadb, postgres or leave for a default (sqlite for one-server-node installs, embedded etcd otherwise)"
+  default     = null
+}
+
+variable "allocated_storage_gb" {
+  description = "Size of DB allocated storage"
+  default     = 20
+}
+
+variable "username" {
+  description = "The database's main user name"
+  default     = "admin"
+}
+
+variable "password" {
+  description = "The database's main user password"
+  default     = "v3ryverysecre7"
+}
