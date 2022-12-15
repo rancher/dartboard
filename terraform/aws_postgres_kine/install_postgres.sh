4c1daad2b3a2e820c55aa03acf723b903eba8667
Silvio Moioli <silvio@moioli.net>
2022-12-15 16:18:51 +0100

    Add module to install a Postgres VM with kine
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ terraform/aws_postgres_kine/install_postgres.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# Install the repository
+mkdir -p /etc/yum.repos.d/
+cat >/etc/yum.repos.d/pg.repo <<EOF
+[pg14]
+name=PostgreSQL 14 for RHEL/CentOS 7 - aarch64
+baseurl=https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-7-aarch64
+enabled=1
+gpgcheck=0
+EOF
+
+# Install PostgreSQL
+yum install -y postgresql14 postgresql14-server
+
+# Initialize the database and enable automatic start
+/usr/pgsql-14/bin/postgresql-14-setup initdb
+systemctl enable postgresql-14
+systemctl start postgresql-14
+
+# Create kine user
+su - postgres -c psql <<EOF
+  CREATE USER kineuser WITH PASSWORD 'kinepassword';
+  CREATE DATABASE kine;
+  GRANT ALL ON DATABASE kine TO kineuser;
+EOF
+
+# Install kine
+curl -L -o /usr/bin/kine https://github.com/k3s-io/kine/releases/download/v0.9.8/kine-arm64
+chmod +x /usr/bin/kine
+
+cat >/etc/systemd/system/kine.service <<EOF
+[Unit]
+Description=kine
+
+[Service]
+ExecStart=/usr/bin/kine --endpoint postgres://kineuser:kinepassword@localhost:5432/kine?sslmode=disable
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+
+# Start kine
+systemctl enable kine
+systemctl start kine
---
+++ terraform/aws_postgres_kine/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    ssh = {
+      source = "loafoe/ssh"
+    }
+  }
+}
+
+module "server_node" {
+  source                = "../aws_host"
+  ami                   = var.ami
+  instance_type         = var.instance_type
+  availability_zone     = var.availability_zone
+  project_name          = var.project_name
+  name                  = "${var.name}-server"
+  ssh_key_name          = var.ssh_key_name
+  ssh_private_key_path  = var.ssh_private_key_path
+  subnet_id             = var.subnet_id
+  vpc_security_group_id = var.vpc_security_group_id
+  ssh_bastion_host      = var.ssh_bastion_host
+}
+
+resource "ssh_resource" "install_postgres" {
+  depends_on = [module.server_node]
+
+  host         = module.server_node.private_name
+  private_key  = file(var.ssh_private_key_path)
+  user         = "root"
+  bastion_host = var.ssh_bastion_host
+
+  file {
+    content     = templatefile("${path.module}/install_postgres.sh", {})
+    destination = "/root/install_postgres.sh"
+    permissions = "0700"
+  }
+
+  commands = [
+    "/root/install_postgres.sh > >(tee install_postgres.log) 2> >(tee install_postgres.err >&2)"
+  ]
+}
+
+// connect with
+// PGPASSWORD=kinepassword psql -U kineuser -h localhost kine
+
+output "private_name" {
+  value = module.server_node.private_name
+}
