0fc039b69ceaf8f9fad1afbc8419265aff60b28c
Silvio Moioli <silvio@moioli.net>
2022-12-16 10:50:20 +0100

    Adapt main.tf to test
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -2,6 +2,7 @@ locals {
   upstream_server_count   = 3
   upstream_agent_count    = 0
   upstream_distro_version = "v1.24.6+k3s1"
+  upstream_kine_image     = "rancher/kine:faster-list"
   rancher_chart           = "https://releases.rancher.com/server-charts/latest/rancher-2.6.9.tgz"
   upstream_san            = "upstream.local.gd"
   upstream_datastore      = "postgres"
--- terraform/main.tf
+++ terraform/main.tf
@@ -26,30 +26,17 @@ module "network" {
 }
 
 module "upstream_cluster" {
-  source                   = "./k3d_k3s"
-  project_name             = local.project_name
-  name                     = "upstream"
-  network_name             = module.network.name
-  server_count             = local.upstream_server_count
-  agent_count              = local.upstream_agent_count
-  distro_version           = local.upstream_distro_version
-  sans                     = [local.upstream_san]
-  datastore                = local.upstream_datastore
-  enable_pprof             = local.upstream_enable_pprof
-  additional_port_mappings = [[3000, 443], [6443, 6443]]
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-module "rancher" {
-  depends_on   = [module.upstream_cluster]
-  count        = local.upstream_server_count > 0 ? 1 : 0
-  source       = "./rancher"
-  public_name  = local.upstream_san
-  private_name = module.upstream_cluster.first_server_private_name
-  chart        = local.rancher_chart
+  source                              = "./k3d_k3s"
+  project_name                        = local.project_name
+  name                                = "upstream"
+  network_name                        = module.network.name
+  server_count                        = local.upstream_server_count
+  agent_count                         = local.upstream_agent_count
+  distro_version                      = local.upstream_distro_version
+  kine_image                          = local.upstream_kine_image
+  sans                                = [local.upstream_san]
+  datastore                           = local.upstream_datastore
+  enable_pprof                        = local.upstream_enable_pprof
+  postgres_log_min_duration_statement = 1000
+  additional_port_mappings            = [[3000, 443], [6443, 6443]]
 }
