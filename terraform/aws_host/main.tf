22d6fcac9a7215f0bc04663101117042bceb2cd5
Silvio Moioli <silvio@moioli.net>
2023-01-11 14:11:02 +0100

    aws_host: also return raw IP
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/aws_host/main.tf
+++ terraform/aws_host/main.tf
@@ -80,6 +80,10 @@ output "private_name" {
   value = aws_instance.instance.private_dns
 }
 
+output "private_ip" {
+  value = aws_instance.instance.private_ip
+}
+
 output "public_name" {
   depends_on = [null_resource.host_configuration]
   value      = aws_instance.instance.public_dns
