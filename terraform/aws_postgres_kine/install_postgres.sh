438185cbb2ff132e88cf62e386b4b4966b510a8f
Silvio Moioli <silvio@moioli.net>
2023-01-12 14:54:14 +0100

    aws_postgres_kine: add minimal tuning params
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/aws_postgres_kine/install_postgres.sh
+++ terraform/aws_postgres_kine/install_postgres.sh
@@ -17,6 +17,37 @@ yum install -y postgresql15 postgresql15-server
 
 # Initialize the database and enable automatic start
 ls /var/lib/pgsql/15/initdb.log || /usr/pgsql-15/bin/postgresql-15-setup initdb
+
+# Set basic tuning parameters
+cat >>/var/lib/pgsql/15/data/postgresql.conf <<EOF
+# Tuning parameters from https://pgtune.leopard.in.ua/ based on instance type m6id.4xlarge
+# DB Version: 15
+# OS Type: linux
+# DB Type: web
+# Total Memory (RAM): 64 GB
+# CPUs num: 16
+# Connections num: 50
+# Data Storage: ssd
+
+max_connections = 50
+shared_buffers = 16GB
+effective_cache_size = 48GB
+maintenance_work_mem = 2GB
+checkpoint_completion_target = 0.9
+wal_buffers = 16MB
+default_statistics_target = 100
+random_page_cost = 1.1
+effective_io_concurrency = 200
+work_mem = 83886kB
+min_wal_size = 1GB
+max_wal_size = 4GB
+max_worker_processes = 16
+max_parallel_workers_per_gather = 4
+max_parallel_workers = 16
+max_parallel_maintenance_workers = 4
+EOF
+
+# Start DB
 systemctl enable postgresql-15
 systemctl start postgresql-15
 
