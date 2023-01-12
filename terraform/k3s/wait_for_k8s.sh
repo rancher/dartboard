c1fcf18447ae4c3d903cfead65983e981b818554
Silvio Moioli <silvio@moioli.net>
2023-01-12 10:54:30 +0100

    k3s: time out after waiting for API for 2 minutes
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/k3s/wait_for_k8s.sh
+++ terraform/k3s/wait_for_k8s.sh
@@ -2,8 +2,15 @@
 
 set -xe
 
-while ! kubectl get services
+for i in {1..20}
 do
+  if kubectl get services
+  then
+    exit 0
+  fi
   echo "Waiting for k8s API to be up..."
-  sleep 3
+  sleep 2
 done
+
+echo "ERROR: k8s API still not up after 20 attempts, quitting"
+exit 1
