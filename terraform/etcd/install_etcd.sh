557d6aea1cbe8d1ca738d5e323cb131b78635b06
Silvio Moioli <silvio@moioli.net>
2023-01-17 12:17:19 +0100

    HACK: install on SSD partitions
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/etcd/install_etcd.sh
+++ terraform/etcd/install_etcd.sh
@@ -8,6 +8,19 @@ DOWNLOAD_URL=https://github.com/etcd-io/etcd/releases/download
 curl -L $DOWNLOAD_URL/$ETCD_VER/etcd-$ETCD_VER-linux-amd64.tar.gz -o /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz
 tar xzvf /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz -C /usr/bin --strip-components=1
 
+# if this host has a local SSD device (eg. m6i family), format the partition and mount the data path on it
+if [ -b /dev/nvme1n1 ] && [ ! -b /dev/nvme1n1p1 ]; then
+  /usr/sbin/parted -s /dev/nvme1n1 mklabel gpt
+  /usr/sbin/parted -s /dev/nvme1n1 mkpart primary 0% 100%
+
+  # HACK: allow kernel time before calling mkfs
+  sleep 1
+
+  /sbin/mkfs.xfs /dev/nvme1n1p1
+  mkdir -p /var/lib/etcd
+  mount /dev/nvme1n1p1 /var/lib/etcd
+fi
+
 cat >/etc/systemd/system/etcd.service <<EOF
 [Unit]
 Description=etcd
@@ -21,7 +34,8 @@ ExecStart=/usr/bin/etcd \
   --initial-advertise-peer-urls http://${server_ip}:2380 \
   --initial-cluster-token ${etcd_token} \
   --initial-cluster ${join(",", formatlist("%s=http://%s:2380", etcd_names, server_names))} \
-  --initial-cluster-state new
+  --initial-cluster-state new \
+  --data-dir /var/lib/etcd
 
 [Install]
 WantedBy=multi-user.target
