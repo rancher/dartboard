bd0eb0b739b42323b1d497c8696f71f39898b9f6
Silvio Moioli <silvio@moioli.net>
2023-01-25 12:12:04 +0100

    Vendor load test dependencies
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ k6/lib/refresh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+set -xe
+
+curl -o papaparse-5.3.2.js https://raw.githubusercontent.com/mholt/PapaParse/5.3.2/papaparse.js
+curl -o js-yaml-4.1.0.mjs https://raw.githubusercontent.com/nodeca/js-yaml/4.1.0/dist/js-yaml.mjs
+curl -o k6-summary-0.0.2.js https://jslib.k6.io/k6-summary/0.0.2/index.js
+curl -o url-1.0.0.js https://jslib.k6.io/url/1.0.0/index.js
