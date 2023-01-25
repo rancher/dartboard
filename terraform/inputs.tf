980f1585a31f3b4165511f43b8ee68d8d1f6a3b1
Silvio Moioli <silvio@moioli.net>
2023-01-25 13:57:54 +0100

    Adapt main.tf
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -1,11 +1,16 @@
+variable "upstream_datastore" {
+  type = string
+}
+
+variable "kine_image" {
+  type = string
+}
+
 locals {
   upstream_server_count   = 3
   upstream_agent_count    = 0
   upstream_distro_version = "v1.24.6+k3s1"
-  rancher_chart           = "https://releases.rancher.com/server-charts/latest/rancher-2.6.9.tgz"
   upstream_san            = "upstream.local.gd"
-  upstream_datastore      = "postgres"
-  upstream_enable_pprof   = true
 
   project_name = "moio"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -26,30 +26,14 @@ module "network" {
 }
 
 module "upstream_cluster" {
-  source                   = "./k3d_k3s"
-  project_name             = local.project_name
-  name                     = "upstream"
-  network_name             = module.network.name
-  server_count             = local.upstream_server_count
-  agent_count              = local.upstream_agent_count
-  distro_version           = local.upstream_distro_version
-  sans                     = [local.upstream_san]
-  datastore                = local.upstream_datastore
-  enable_pprof             = local.upstream_enable_pprof
-  additional_port_mappings = [[3000, 443], [6443, 6443]]
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-module "rancher" {
-  depends_on   = [module.upstream_cluster]
-  count        = local.upstream_server_count > 0 ? 1 : 0
-  source       = "./rancher"
-  public_name  = local.upstream_san
-  private_name = module.upstream_cluster.first_server_private_name
-  chart        = local.rancher_chart
+  source         = "./k3d_k3s"
+  project_name   = local.project_name
+  name           = "upstream"
+  network_name   = module.network.name
+  server_count   = local.upstream_server_count
+  agent_count    = local.upstream_agent_count
+  distro_version = local.upstream_distro_version
+  sans           = [local.upstream_san]
+  datastore      = var.upstream_datastore
+  kine_image     = var.kine_image
 }
