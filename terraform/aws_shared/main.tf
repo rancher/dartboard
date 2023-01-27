0931c1738227a578155c3f489ff60b4818f6bed9
Silvio Moioli <silvio@moioli.net>
2023-01-27 15:34:14 +0100

    aws_shared: remove unused ami lookups
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/aws_shared/main.tf
+++ terraform/aws_shared/main.tf
@@ -1,47 +1,3 @@
-// AMI lookups
-
-data "aws_ami" "sles15sp4" {
-  most_recent = true
-  name_regex  = "^suse-sles-15-sp4-byos-v"
-  owners      = ["013907871322"]
-
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
-data "aws_ami" "rocky8" {
-  most_recent = true
-  name_regex  = "Rocky-8"
-  owners      = ["792107900819"]
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
 resource "aws_key_pair" "key_pair" {
   key_name   = "${var.project_name}-key-pair"
   public_key = file(var.ssh_public_key_path)
@@ -54,12 +10,4 @@ resource "aws_key_pair" "key_pair" {
 
 output "key_name" {
   value = aws_key_pair.key_pair.key_name
-}
-
-output "latest_sles_ami" {
-  value = data.aws_ami.sles15sp4
-}
-
-output "latest_rocky_ami" {
-  value = data.aws_ami.rocky8
 }
