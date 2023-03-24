11ee09f47d0452cc5afb79012faa318d8c86534b
Silvio Moioli <silvio@moioli.net>
2023-03-24 15:53:00 +0100

    Drop Cypress code altogether
    Setup will be handled via k6 instead
    
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- cypress/cypress/support/e2e.js
+++
@@ -1,9 +0,0 @@
-// This is processed and loaded automatically before test files
-
-import './commands'
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-})
