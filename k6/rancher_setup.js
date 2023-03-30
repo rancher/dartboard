176830261ff0ab543a6a899a71abafef3c08ece3
Silvio Moioli <silvio@moioli.net>
2023-03-30 13:15:43 +0200

    Bugfix: use the correct variable name
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- k6/rancher_setup.js
+++ k6/rancher_setup.js
@@ -7,7 +7,7 @@ export const options = {
 
 export default function main() {
     const baseUrl = __ENV.BASE_URL
-    const bootstrapPassword = __ENV.BOOSTRAP_PASSWORD
+    const bootstrapPassword = __ENV.BOOTSTRAP_PASSWORD
     const password = __ENV.PASSWORD
     const importedClusterNames = __ENV.IMPORTED_CLUSTER_NAMES.split(",")
 
