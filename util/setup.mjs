8a31c59ad8c22ba31cceb284b124b9f2c61d4990
Silvio Moioli <silvio@moioli.net>
2023-04-05 16:55:17 +0200

    refactoring: do not cd, use command arguments instead
    Benefit: commands are copy-pastable from logs
    
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- util/setup.mjs
+++ util/setup.mjs
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
-import {ADMIN_PASSWORD, cd, run, runCollectingJSONOutput, runCollectingOutput} from "./common.mjs"
+import {ADMIN_PASSWORD, dir, run, runCollectingJSONOutput, runCollectingOutput, sleep} from "./common.mjs"
 
-cd("terraform")
 
-run("terraform init")
-run("terraform apply -auto-approve")
+run(`terraform -chdir=${dir("terraform")} init`)
+run(`terraform -chdir=${dir("terraform")} apply -auto-approve`)
 
-const params = runCollectingJSONOutput("terraform output -json")
+const params = runCollectingJSONOutput(`terraform -chdir=${dir("terraform")} output -json`)
 const baseUrl = params["base_url"]["value"]
 const bootstrapPassword = params["bootstrap_password"]["value"]
 const upstreamCluster = params["upstream_cluster"]["value"]
 const importedClusters = params["downstream_clusters"]["value"]
 const importedClusterNames = importedClusters.map(c => c["name"])
 
-cd("k6")
-run(`k6 run -e BASE_URL=${baseUrl} -e BOOTSTRAP_PASSWORD=${bootstrapPassword} -e PASSWORD=${ADMIN_PASSWORD} -e IMPORTED_CLUSTER_NAMES=${importedClusterNames} ./rancher_setup.js`)
+run(`k6 run -e BASE_URL=${baseUrl} -e BOOTSTRAP_PASSWORD=${bootstrapPassword} -e PASSWORD=${ADMIN_PASSWORD} -e IMPORTED_CLUSTER_NAMES=${importedClusterNames} ${dir("k6")}/rancher_setup.js`)
 
 const uka = upstreamCluster["kubeconfig"] ? ` --kubeconfig=${upstreamCluster["kubeconfig"]}` : ""
 const uca = upstreamCluster["context"] ? ` --context=${upstreamCluster["context"]}` : ""
