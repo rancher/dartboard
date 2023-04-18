9709809110241d385a47625944c444195bdd43bf
Silvio Moioli <silvio@moioli.net>
2023-04-18 09:24:40 +0200

    Bugfix: make sure tunnels work for bastion host as well
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/aws_host/open-tunnels-to.sh
+++ terraform/aws_host/open-tunnels-to.sh
@@ -10,8 +10,8 @@ nohup ssh -o IgnoreUnknown=TerraformCreatedThisTunnel \
   -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   -N \
   %{ for tunnel in ssh_tunnels }-L ${tunnel[0]}:localhost:${tunnel[1]} %{ endfor }\
-  %{ if ssh_bastion_host != null }-o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${ssh_bastion_host}"%{ endif }\
-  root@${private_name} >/dev/null 2>&1 &
+  %{ if ssh_bastion_host != null ~}-o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${ssh_bastion_host}"\%{ endif ~}
+  root@${ssh_bastion_host != null ? private_name : public_name} >/dev/null 2>&1 &
 
 %{ for tunnel in ssh_tunnels }
 echo "Waiting for tunnel ${tunnel[0]} to be up..."
