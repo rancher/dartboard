662ce31837e8633a6556d97e1304c9f8a518db8b
Silvio Moioli <silvio@moioli.net>
2023-05-29 15:16:34 +0200

    Refactoring: drive terraform cluster creation via a map
    This makes the cluster count/assortment more flexible - no edits to outputs needed, for example.
    
    This patch also adds a new cluster called "tester", which is used to house the k6 load generator,
    the overall-test mimir database and an own grafana instance.
    
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -1,17 +1,43 @@
 locals {
-  upstream_server_count        = 3
-  upstream_agent_count         = 2
-  upstream_distro_version      = "v1.24.12+k3s1"
-  upstream_san                 = "upstream.local.gd"
-  upstream_kubernetes_api_port = 6445
-
-  upstream_public_port = 8443
-
-  downstream_server_count        = 1
-  downstream_agent_count         = 0
-  downstream_distro_version      = "v1.24.12+k3s1"
-  downstream_san                 = "downstream.local.gd"
-  downstream_kubernetes_api_port = 6446
-
   project_name = "moio"
+
+  clusters = {
+    upstream : {
+      server_count   = 3
+      agent_count    = 2
+      distro_version = "v1.24.12+k3s1"
+      labels = [
+        { key : "monitoring", value : "true", node_filters : ["agent:0"] }
+      ]
+      taints = [
+        { key : "monitoring", value : "true", effect : "NoSchedule", node_filters : ["agent:0"] }
+      ]
+      san                 = "upstream.local.gd"
+      kubernetes_api_port = 6445
+      public_http_port    = 8080
+      public_https_port   = 8443
+    },
+    downstream : {
+      server_count        = 1
+      agent_count         = 0
+      distro_version      = "v1.24.12+k3s1"
+      labels              = []
+      taints              = []
+      san                 = "downstream.local.gd"
+      kubernetes_api_port = 6446
+      public_http_port    = 8081
+      public_https_port   = 8444
+    },
+    tester : {
+      server_count        = 1
+      agent_count         = 0
+      distro_version      = "v1.24.12+k3s1"
+      labels              = []
+      taints              = []
+      san                 = "tester.local.gd"
+      kubernetes_api_port = 6447
+      public_http_port    = 8082
+      public_https_port   = 8445
+    },
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -17,29 +17,18 @@ module "network" {
   project_name = local.project_name
 }
 
-module "upstream_cluster" {
+module "cluster" {
+  for_each                 = local.clusters
   source                   = "./k3d_k3s"
   project_name             = local.project_name
-  name                     = "upstream"
+  name                     = each.key
   network_name             = module.network.name
-  server_count             = local.upstream_server_count
-  agent_count              = local.upstream_agent_count
-  labels                   = [{ key : "monitoring", value : "true", node_filters : ["agent:0"] }]
-  taints                   = [{ key : "monitoring", value : "true", effect : "NoSchedule", node_filters : ["agent:0"] }]
-  distro_version           = local.upstream_distro_version
-  sans                     = [local.upstream_san]
-  kubernetes_api_port      = local.upstream_kubernetes_api_port
-  additional_port_mappings = [[local.upstream_public_port, 443]]
-}
-
-module "downstream_cluster" {
-  source              = "./k3d_k3s"
-  project_name        = local.project_name
-  name                = "downstream"
-  network_name        = module.network.name
-  server_count        = local.downstream_server_count
-  agent_count         = local.downstream_agent_count
-  distro_version      = local.downstream_distro_version
-  sans                = [local.downstream_san]
-  kubernetes_api_port = local.downstream_kubernetes_api_port
+  server_count             = each.value.server_count
+  agent_count              = each.value.agent_count
+  labels                   = each.value.labels
+  taints                   = each.value.taints
+  distro_version           = each.value.distro_version
+  sans                     = [each.value.san]
+  kubernetes_api_port      = each.value.kubernetes_api_port
+  additional_port_mappings = [[each.value.public_http_port, 80], [each.value.public_https_port, 443]]
 }
