bf2935458e1378451baa3e55240a5beacff2f6ed
Silvio Moioli <silvio@moioli.net>
2023-05-29 15:16:34 +0200

    Refactoring: drive terraform cluster creation via a map
    This makes the cluster count/assortment more flexible - no edits to outputs needed, for example.
    
    This patch also adds a new cluster called "tester", which is used to house the k6 load generator,
    the overall-test mimir database and an own grafana instance.
    
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,14 @@
-output "upstream_san" {
-  value = local.upstream_san
-}
-
-output "upstream_public_port" {
-  value = local.upstream_public_port
-}
-
-output "upstream_cluster_private_name" {
-  value = module.upstream_cluster.first_server_private_name
-}
-
-output "upstream_cluster" {
-  value = { kubeconfig : pathexpand("~/.kube/config"), context : "k3d-${local.project_name}-upstream" }
-}
-
-output "downstream_clusters" {
-  value = [
-    { name : "downstream", kubeconfig : pathexpand("~/.kube/config"), context : "k3d-${local.project_name}-downstream" }
-  ]
+output "clusters" {
+  value = {
+    for name, locals in local.clusters : name => {
+      name : name,
+      san : locals.san,
+      public_http_port : locals.public_http_port,
+      public_https_port : locals.public_https_port,
+      private_name        = module.cluster[name].first_server_private_name,
+      kubernetes_api_port = locals.kubernetes_api_port
+      kubeconfig          = pathexpand("~/.kube/config")
+      context             = "k3d-${local.project_name}-${name}"
+    }
+  }
 }
