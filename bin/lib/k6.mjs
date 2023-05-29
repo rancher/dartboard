a42134f7dde4268a6d9ab4fdde37c8e45140b9ee
Silvio Moioli <silvio@moioli.net>
2023-05-29 15:31:15 +0200

    k6: use own namespace, transfer kubeconfig if needed, add support to tags, make recording optional
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- bin/lib/k6.mjs
+++ bin/lib/k6.mjs
@@ -1,15 +1,30 @@
-import {dir, q, run} from "./common.mjs";
+import {dir, q, run} from "./common.mjs"
 
-const RANCHER_URL = "https://rancher.cattle-system"
-const MIMIR_URL = "http://mimir.cattle-monitoring-system:9009/api/v1/push"
+const MIMIR_URL = "http://mimir.tester:9009/mimir"
 
-/** Runs k6 from inside the k8s cluster */
-export function k6_run(envs, test) {
-    envs["BASE_URL"] = RANCHER_URL
-    const cmdline = "k6 run " + Object.entries(envs).map(([k,v]) => `-e ${q(k)}=${q(v)}`).join(" ") + " " + q(dir(test))
+
+/**
+ * Runs a k6 script from inside the specified k8s cluster via kubectl run.
+ * Specify envs, tags, and a test file from the k6/ dir (which will be transfered via ConfigMaps).
+ * Set record = true for result metrics to be sent to Mimir, which is expected to be running in the same cluster.
+ *
+ * If the test script exercises the Kubernetes API, specify a KUBECONFIG env, the corresponding file will be transferred
+ * to the cluster via a Secret.
+ */
+export function k6_run(cluster, envs, tags, test, record = false) {
+    if (envs["KUBECONFIG"]) {
+        run(`kubectl --namespace=tester delete secret kube --ignore-not-found --kubeconfig=${q(cluster["kubeconfig"])} --context=${q(cluster["context"])}`)
+        run(`kubectl --namespace=tester create secret generic kube --from-file=config=${envs["KUBECONFIG"]} --kubeconfig=${q(cluster["kubeconfig"])} --context=${q(cluster["context"])}`)
+        envs["KUBECONFIG"] = "/kube/config"
+    }
+
+    const envArgs = Object.entries(envs).map(([k,v]) => ["-e", `${k}=${v}`]).flat()
+    const tagArgs = Object.entries(tags).map(([k,v]) => ["--tag", `${k}=${v}`]).flat()
+    const outputArgs = record ? ["-o", "experimental-prometheus-rw"] : []
+
+    const cmdline = `k6 run ${envArgs.join(" ")} ${tagArgs.join(" ")} ${q(dir(test))}`
     console.log(`***Running equivalent of:\n ${cmdline}\n`)
 
-    const args = Object.entries(envs).map(([k,v]) => ["-e", `${k}=${v}`]).flat()
     const overrides = {
         "apiVersion": "v1",
         "spec": {
@@ -19,39 +34,25 @@ export function k6_run(envs, test) {
                     "image": "grafana/k6:0.44.1",
                     "stdin": true,
                     "tty": true,
-                    "args": [ "run" , args, test, "-o", "experimental-prometheus-rw"].flat(),
+                    "args": [ "run" , envArgs, tagArgs, test, outputArgs].flat(),
                     "workingDir": "/",
                     "env": [
-                        {"name": "K6_PROMETHEUS_RW_SERVER_URL", "value": MIMIR_URL},
+                        {"name": "K6_PROMETHEUS_RW_SERVER_URL", "value": MIMIR_URL + "/api/v1/push"},
+                        {"name": "K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM", "value": "true"},
+                        {"name": "K6_PROMETHEUS_RW_STALE_MARKERS", "value": "true"},
                     ],
                     "volumeMounts": [
-                        {
-                            "mountPath": "/k6",
-                            "name": "k6-test-files"
-                        },
-                        {
-                            "mountPath": "/k6/lib",
-                            "name": "k6-lib-files"
-                        }
-                    ]
+                        { "mountPath": "/k6", "name": "k6-test-files" },
+                        { "mountPath": "/k6/lib", "name": "k6-lib-files"}
+                    ].concat(envs["KUBECONFIG"] ? [{ "mountPath": "/kube", "name": "kube" }]: [])
                 },
             ],
             "volumes": [
-                {
-                    "name": "k6-test-files",
-                    "configMap": {
-                        "name": "k6-test-files"
-                    }
-                },
-                {
-                    "name": "k6-lib-files",
-                    "configMap": {
-                        "name": "k6-lib-files"
-                    }
-                }
-            ]
+                { "name": "k6-test-files", "configMap": { "name": "k6-test-files" } },
+                { "name": "k6-lib-files", "configMap": { "name": "k6-lib-files" } }
+            ].concat(envs["KUBECONFIG"] ? [ { "name": "kube", "secret": { "secretName": "kube" } } ] : [])
         }
     }
 
-    return run(`kubectl run k6 --image grafana/k6:0.44.1 --namespace=cattle-monitoring-system --rm -i --tty --restart=Never --overrides=${q(JSON.stringify(overrides))}`)
+    return run(`kubectl run k6 --image grafana/k6:0.44.1 --namespace=tester --rm -i --tty --restart=Never --overrides=${q(JSON.stringify(overrides))} --kubeconfig=${q(cluster["kubeconfig"])} --context=${q(cluster["context"])}`)
 }
