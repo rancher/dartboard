833bd0d06b8029f155307e5515f82a37c654b19b
Silvio Moioli <silvio@moioli.net>
2023-06-12 09:32:45 +0200

    aws_network: refactoring: split inputs/main/outputs
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/aws_network/main.tf
+++ terraform/aws_network/main.tf
@@ -226,27 +226,3 @@ resource "aws_key_pair" "key_pair" {
     Name    = "${var.project_name}-ssh-key-pair"
   }
 }
-
-output "key_name" {
-  value = aws_key_pair.key_pair.key_name
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-
-output "secondary_private_subnet_id" {
-  value = var.secondary_availability_zone != null ? aws_subnet.secondary_private[0].id : null
-}
-
-output "public_security_group_id" {
-  value = aws_security_group.public.id
-}
-
-output "private_security_group_id" {
-  value = aws_security_group.private.id
-}
