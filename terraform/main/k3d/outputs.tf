4f3543e986c4255260b52dca2961e2018fbcaf0b
Silvio Moioli <silvio@moioli.net>
2023-06-12 17:31:03 +0200

    drop kubernetes_api_port, unused
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/main/k3d/outputs.tf
+++ terraform/main/k3d/outputs.tf
@@ -5,10 +5,9 @@ output "clusters" {
     san : locals.san,
     public_http_port : locals.public_http_port,
     public_https_port : locals.public_https_port,
-    private_name        = module.cluster[name].first_server_private_name,
-    kubernetes_api_port = locals.kubernetes_api_port
-    kubeconfig          = module.cluster[name].kubeconfig
-    context             = module.cluster[name].context
+    private_name = module.cluster[name].first_server_private_name,
+    kubeconfig   = module.cluster[name].kubeconfig
+    context      = module.cluster[name].context
   }
   }
 }
