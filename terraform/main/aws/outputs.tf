ad0f7900c7d782cc30111acaf0eef102461585c0
Silvio Moioli <silvio@moioli.net>
2023-06-13 15:19:05 +0200

    Add AWS platform support
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ terraform/main/aws/outputs.tf
@@ -0,0 +1,15 @@
+output "clusters" {
+  value = {
+  for i, cluster in local.clusters : cluster.name => {
+    local_name : cluster.local_name,
+    local_http_port : module.cluster[i].local_http_port,
+    local_https_port : module.cluster[i].local_https_port,
+    private_name         = module.cluster[i].first_server_private_name,
+    kubeconfig           = module.cluster[i].kubeconfig
+    context              = module.cluster[i].context
+    node_access_commands = {
+    for node_name, filename in module.cluster[i].ssh_scripts : node_name => filename
+    }
+  }
+  }
+}
