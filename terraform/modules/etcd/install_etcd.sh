71a3ab0b41a5ec957d478dee6c0bf4dde913aab9
Silvio Moioli <silvio@moioli.net>
2023-06-21 13:10:06 +0200

    aws: extend ssd usage mechanism
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/modules/etcd/install_etcd.sh
+++ terraform/modules/etcd/install_etcd.sh
@@ -8,18 +8,11 @@ DOWNLOAD_URL=https://github.com/etcd-io/etcd/releases/download
 curl -L $DOWNLOAD_URL/$ETCD_VER/etcd-$ETCD_VER-linux-amd64.tar.gz -o /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz
 tar xzvf /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz -C /usr/bin --strip-components=1
 
-# if this host has a local SSD device (eg. m6i family), format the partition and mount the data path on it
-if [ -b /dev/nvme1n1 ] && [ ! -b /dev/nvme1n1p1 ]; then
-  /usr/sbin/parted -s /dev/nvme1n1 mklabel gpt
-  /usr/sbin/parted -s /dev/nvme1n1 mkpart primary 0% 100%
-
-  # HACK: allow kernel time before calling mkfs
-  sleep 1
-
-  /sbin/mkfs.xfs /dev/nvme1n1p1
-  mkdir -p /var/lib/etcd
-  mount /dev/nvme1n1p1 /var/lib/etcd
-fi
+# use data disk if available (see mount_ephemeral.sh)
+if [ -d /data ]; then
+  mkdir -p /data/etcd
+  ln -sf /data/etcd /var/lib/etcd
+endif
 
 cat >/etc/systemd/system/etcd.service <<EOF
 [Unit]
