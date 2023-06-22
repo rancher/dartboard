8f38fd787440492f1e8f214a41302365b6fad2e2
Silvio Moioli <silvio@moioli.net>
2023-06-22 12:01:30 +0200

    Refactoring: make number of downstream clusters easily configurable
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/main/aws/inputs.tf
+++ terraform/main/aws/inputs.tf
@@ -1,51 +1,56 @@
 locals {
   project_name = "moio"
 
-  clusters = [
-    {
-      name           = "upstream"
-      server_count   = 3
-      agent_count    = 2
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = [
-        [{ key : "monitoring", value : "true" }]
-      ]
-      agent_taints = [
-        [{ key : "monitoring", value : "true", effect : "NoSchedule" }]
-      ]
+  upstream_cluster = {
+    name           = "upstream"
+    server_count   = 3
+    agent_count    = 2
+    distro_version = "v1.24.12+k3s1"
+    agent_labels   = [
+      [{ key : "monitoring", value : "true" }]
+    ]
+    agent_taints = [
+      [{ key : "monitoring", value : "true", effect : "NoSchedule" }]
+    ]
 
-      // aws-specific
-      local_name    = "upstream.local.gd"
-      instance_type = "i3.large"
-      ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
-    },
-    {
-      name           = "downstream"
-      server_count   = 1
-      agent_count    = 0
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = []
-      agent_taints   = []
+    // aws-specific
+    local_name    = "upstream.local.gd"
+    instance_type = "i3.large"
+    ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
+  }
 
-      // aws-specific
-      local_name    = "downstream.local.gd"
-      instance_type = "t4g.large"
-      ami           = "ami-0e55a8b472a265e3f" // openSUSE-Leap-15-5-v20230608-hvm-ssd-arm64
-    },
-    {
-      name           = "tester"
-      server_count   = 1
-      agent_count    = 0
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = []
-      agent_taints   = []
+  downstream_clusters = [
+  for i in range(1) :
+  {
+    name           = "downstream-${i}"
+    server_count   = 1
+    agent_count    = 0
+    distro_version = "v1.24.12+k3s1"
+    agent_labels   = []
+    agent_taints   = []
 
-      // aws-specific
-      local_name    = "tester.local.gd"
-      instance_type = "t3a.large"
-      ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
-    },
+    // aws-specific
+    local_name    = "downstream-${i}.local.gd"
+    instance_type = "t4g.large"
+    ami           = "ami-0e55a8b472a265e3f" // openSUSE-Leap-15-5-v20230608-hvm-ssd-arm64
+  }
   ]
+
+  tester_cluster = {
+    name           = "tester"
+    server_count   = 1
+    agent_count    = 0
+    distro_version = "v1.24.12+k3s1"
+    agent_labels   = []
+    agent_taints   = []
+
+    // aws-specific
+    local_name    = "tester.local.gd"
+    instance_type = "t3a.large"
+    ami           = "ami-009fd8a4732ea789b" // openSUSE-Leap-15-5-v20230608-hvm-ssd-x86_64
+  }
+
+  clusters = concat([local.upstream_cluster], local.downstream_clusters, [local.tester_cluster])
 
   // aws-specific
   first_local_kubernetes_api_port = 7445
--- terraform/main/k3d/inputs.tf
+++ terraform/main/k3d/inputs.tf
@@ -1,45 +1,50 @@
 locals {
   project_name = "moio"
 
-  clusters = [
-    {
-      name           = "upstream"
-      server_count   = 3
-      agent_count    = 2
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = [
-        [{ key : "monitoring", value : "true" }]
-      ]
-      agent_taints = [
-        [{ key : "monitoring", value : "true", effect : "NoSchedule" }]
-      ]
+  upstream_cluster = {
+    name           = "upstream"
+    server_count   = 3
+    agent_count    = 2
+    distro_version = "v1.24.12+k3s1"
+    agent_labels   = [
+      [{ key : "monitoring", value : "true" }]
+    ]
+    agent_taints = [
+      [{ key : "monitoring", value : "true", effect : "NoSchedule" }]
+    ]
 
-      // k3d-specific
-      local_name = "upstream.local.gd"
-    },
-    {
-      name           = "downstream"
-      server_count   = 1
-      agent_count    = 0
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = []
-      agent_taints   = []
+    // k3d-specific
+    local_name = "upstream.local.gd"
+  }
 
-      // k3d-specific
-      local_name = "downstream.local.gd"
-    },
-    {
-      name           = "tester"
-      server_count   = 1
-      agent_count    = 0
-      distro_version = "v1.24.12+k3s1"
-      agent_labels   = []
-      agent_taints   = []
+  downstream_clusters = [
+  for i in range(1) :
+  {
+    name           = "downstream-${i}"
+    server_count   = 1
+    agent_count    = 0
+    distro_version = "v1.24.12+k3s1"
+    agent_labels   = []
+    agent_taints   = []
 
-      // k3d-specific
-      local_name = "tester.local.gd"
-    },
+    // k3d-specific
+    local_name = "downstream-${i}.local.gd"
+  }
   ]
+
+  tester_cluster = {
+    name           = "tester"
+    server_count   = 1
+    agent_count    = 0
+    distro_version = "v1.24.12+k3s1"
+    agent_labels   = []
+    agent_taints   = []
+
+    // k3d-specific
+    local_name = "tester.local.gd"
+  }
+
+  clusters = concat([local.upstream_cluster], local.downstream_clusters, [local.tester_cluster])
 
   // k3d-specific
   first_local_kubernetes_api_port = 6445
