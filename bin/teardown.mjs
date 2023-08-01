18991fbe8363059edb19a0759cb4c351afbbe24f
Silvio Moioli <silvio@moioli.net>
2023-08-01 11:44:17 +0200

    Bugfix: do not fail teardown if modules changed since setup
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- bin/teardown.mjs
+++ bin/teardown.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import {terraformDir, q, run} from "./lib/common.mjs"
 
-run(`terraform -chdir=${q(terraformDir())} init`)
+run(`terraform -chdir=${q(terraformDir())} init -upgrade`)
 run(`terraform -chdir=${q(terraformDir())} destroy -auto-approve`)
