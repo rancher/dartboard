df824bdae5587b794cd346f9df035226a5f22f62
Silvio Moioli <silvio@moioli.net>
2023-08-01 11:46:22 +0200

    k3d: add support for docker.io pull proxy registry
    That allows caching re-used images. In one setup, this reduced setup.mjs times from 21 to 11 minutes
    
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/modules/k3d_network/inputs.tf
+++ terraform/modules/k3d_network/inputs.tf
@@ -2,3 +2,8 @@ variable "project_name" {
   description = "A prefix for names of objects created by this module"
   default     = "st"
 }
+
+variable "docker_io_proxy_directory" {
+  description = "Directory in which to save docker.io's pull proxy registry data"
+  default     = "/tmp/docker-io-registry"
+}
