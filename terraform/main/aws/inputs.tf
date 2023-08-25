f699c73e332a0201669273503fad0d333ab6eb88
Silvio Moioli <silvio@moioli.net>
2023-08-25 12:31:09 +0200

    Uniform descriptions of SSH vars
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- terraform/main/aws/inputs.tf
+++ terraform/main/aws/inputs.tf
@@ -62,11 +62,11 @@ locals {
 
 
 variable "ssh_public_key_path" {
-  description = "Path to SSH public key file, see also variable `ssh_private_key_path`."
+  description = "Path to SSH public key file (can be generated with `ssh-keygen -t ed25519`)"
   default     = "~/.ssh/id_ed25519.pub"
 }
 
 variable "ssh_private_key_path" {
-  description = "Path to SSH private key file. (Can be generated with `ssh-keygen -t ed25519`)"
+  description = "Path to SSH private key file (can be generated with `ssh-keygen -t ed25519`)"
   default     = "~/.ssh/id_ed25519"
 }
