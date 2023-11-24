7ddc8f1073a321f1516df288906eba89670e9881
Francesco Giudici <francesco.giudici@suse.com>
2023-11-24 11:14:27 +0100

    azure: add support to mixed kuberntes distros
    Now that we support both k3s and RKE2 deployments, allow to specify as
    input different RKE2/k3s versions.
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- terraform/main/azure/outputs.tf
+++ terraform/main/azure/outputs.tf
@@ -1,13 +1,28 @@
-output "clusters" {
-  value = {
-    for i, cluster in local.clusters : cluster.name => {
-      local_name           = cluster.local_name
-      local_http_port      = module.cluster[i].local_http_port
-      local_https_port     = module.cluster[i].local_https_port
-      private_name         = module.cluster[i].first_server_private_name
-      kubeconfig           = module.cluster[i].kubeconfig
-      context              = module.cluster[i].context
-      node_access_commands = module.cluster[i].node_access_commands
+locals {
+  k3s_outputs = { for i, cluster in local.k3s_clusters : cluster.name => {
+    local_name           = cluster.local_name,
+    local_http_port      = module.k3s_cluster[i].local_http_port
+    local_https_port     = module.k3s_cluster[i].local_https_port
+    private_name         = module.k3s_cluster[i].first_server_private_name
+    public_name          = module.k3s_cluster[i].first_server_public_name
+    kubeconfig           = module.k3s_cluster[i].kubeconfig
+    context              = module.k3s_cluster[i].context
+    node_access_commands = module.k3s_cluster[i].node_access_commands
     }
   }
+  rke2_outputs = { for i, cluster in local.rke2_clusters : cluster.name => {
+    local_name           = cluster.local_name,
+    local_http_port      = module.rke2_cluster[i].local_http_port
+    local_https_port     = module.rke2_cluster[i].local_https_port
+    private_name         = module.rke2_cluster[i].first_server_private_name
+    public_name          = module.rke2_cluster[i].first_server_public_name
+    kubeconfig           = module.rke2_cluster[i].kubeconfig
+    context              = module.rke2_cluster[i].context
+    node_access_commands = module.rke2_cluster[i].node_access_commands
+    }
+  }
+}
+
+output "clusters" {
+  value = merge(local.k3s_outputs, local.rke2_outputs)
 }
