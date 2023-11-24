d44da707bf368420401d04da39e2fdf8c9ac343d
Silvio Moioli <silvio@moioli.net>
2023-11-24 16:43:33 +0100

    azure: add AKS module
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ terraform/modules/azure_aks/input.tf
@@ -0,0 +1,86 @@
+variable "project_name" {
+  description = "A prefix for names of objects created by this module"
+  default     = "st"
+}
+
+variable "name" {
+  description = "Symbolic name of this cluster"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Location where the instance in created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Azure Resource Group name to which the instance should belong"
+  type        = string
+}
+
+variable "system_node_pool_count" {
+  description = "Number of nodes in the default (system) pool for this cluster"
+  default     = 1
+}
+
+variable "main_node_pool_count" {
+  description = "Number of nodes in this cluster's main pool (for workloads)"
+  default     = 0
+}
+
+variable "secondary_node_pool_count" {
+  description = "Number of nodes in this cluster's secondary pool (for workloads)"
+  default     = 0
+}
+
+variable "secondary_node_pool_labels" {
+  description = "Labels to apply to the secondary pool nodes (eg. {key = value})"
+  type        = map(string)
+  default     = {}
+}
+
+variable "secondary_node_pool_taints" {
+  description = "Taints to apply to the secondary pool nodes (eg. ['monitoring=true:NoSchedule'])"
+  type        = list(string)
+  default     = []
+}
+
+variable "os_image" {
+  description = "Azure VM OS image for all nodes in this cluster"
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+}
+
+variable "vm_size" {
+  description = "Azure VM instance type of all nodes in this cluster"
+  default     = "Standard_B2as_v2"
+}
+
+variable "subnet_id" {
+  description = "Azure Subnet id to attach the VM NIC"
+  type        = string
+}
+
+variable "distro_version" {
+  description = "Kubernetes version for AKS to use"
+  default     = "1.26.3"
+}
+
+variable "local_kubernetes_api_port" {
+  description = "Local port this cluster's Kubernetes API will be published to (via SSH tunnel)"
+  default     = 6445
+}
+
+variable "local_http_port" {
+  description = "Local port this cluster's http endpoints will be published to (via SSH tunnel)"
+  default     = 8080
+}
+
+variable "local_https_port" {
+  description = "Local port this cluster's https endpoints will be published to (via SSH tunnel)"
+  default     = 8443
+}
---
+++ terraform/modules/azure_aks/main.tf
@@ -0,0 +1,43 @@
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "${var.project_name}-${var.name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kubernetes_version  = var.distro_version
+  dns_prefix          = "${var.project_name}-${var.name}"
+
+  default_node_pool {
+    name       = "system"
+    node_count = var.system_node_pool_count
+    vm_size    = var.vm_size
+    #    vnet_subnet_id = var.subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "main" {
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
+  name                  = "main"
+  node_count            = var.main_node_pool_count
+  vm_size               = var.vm_size
+  #  vnet_subnet_id        = var.subnet_id
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "secondary" {
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
+  name                  = "secondary"
+  vm_size               = var.vm_size
+  node_count            = var.secondary_node_pool_count
+  node_labels           = var.secondary_node_pool_labels
+  node_taints           = var.secondary_node_pool_taints
+  #  vnet_subnet_id        = var.subnet_id
+}
+
+resource "local_file" "kubeconfig" {
+  content = azurerm_kubernetes_cluster.cluster.kube_config_raw
+
+  filename        = "${path.root}/config/${var.name}.yaml"
+  file_permission = "0700"
+}
