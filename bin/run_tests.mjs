2805c5cf3bfe2bd6a9e7903a53616ad6cbd9e331
Francesco Giudici <francesco.giudici@suse.com>
2023-12-05 16:23:43 +0100

    k6 tests: add more resources
    also invert the order to have user and roles loaded first: this is the
    kind of load more fragile, which used to fail: better to fail earlier
    than later.
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- bin/run_tests.mjs
+++ bin/run_tests.mjs
@@ -13,10 +13,10 @@ import {k6_run} from "./lib/k6.mjs"
 
 
 // Parameters
-const CONFIG_MAP_COUNT = 30_000
-const SECRET_COUNT = 30_000
-const ROLE_COUNT = 100
-const USER_COUNT = 50
+const CONFIG_MAP_COUNT = 10000
+const SECRET_COUNT = 100
+const ROLE_COUNT = 200
+const USER_COUNT = 200
 const PROJECT_COUNT = 50
 
 // Refresh k6 files on the tester cluster
@@ -24,16 +24,10 @@ const clusters = runCollectingJSONOutput(`terraform -chdir=${terraformDir()} out
 const tester = clusters["tester"]
 helm_install("k6-files", dir("charts/k6-files"), tester, "tester", {})
 
-// Create config maps
 const commit = runCollectingOutput("git rev-parse --short HEAD").trim()
 const downstreams = Object.entries(clusters).filter(([k,v]) => k.startsWith("downstream"))
 const upstream = clusters["upstream"]
 
-k6_run(tester,
-    { BASE_URL: upstream["private_kubernetes_api_url"], KUBECONFIG: upstream["kubeconfig"], CONTEXT: upstream["context"], CONFIG_MAP_COUNT: CONFIG_MAP_COUNT, SECRET_COUNT: SECRET_COUNT},
-    {commit: commit, cluster: "upstream", test: "create_load.mjs", ConfigMaps: CONFIG_MAP_COUNT, Secrets: SECRET_COUNT},
-    "k6/create_k8s_resources.js", true
-)
 
 // create users and roles
 const upstreamAddresses = getAppAddressesFor(upstream)
@@ -42,15 +36,20 @@ const rancherClusterNetworkURL = upstreamAddresses.clusterNetwork.httpsURL
 k6_run(tester,
     { BASE_URL: rancherClusterNetworkURL, USERNAME: "admin", PASSWORD: ADMIN_PASSWORD, ROLE_COUNT: ROLE_COUNT, USER_COUNT: USER_COUNT },
     {commit: commit, cluster: "upstream", test: "create_roles_users.mjs", Roles: ROLE_COUNT, Users: USER_COUNT},
-    "k6/create_roles_users.js", true
+    "k6/create_roles_users.js", false
 )
 // create projects
 k6_run(tester,
     { BASE_URL: rancherClusterNetworkURL, USERNAME: "admin", PASSWORD: ADMIN_PASSWORD, PROJECT_COUNT: PROJECT_COUNT },
     {commit: commit, cluster: "upstream", test: "create_projects.mjs", Projects: PROJECT_COUNT},
-    "k6/create_projects.js", true
+    "k6/create_projects.js", false
 )
-
+// Create config maps
+k6_run(tester,
+    { BASE_URL: upstream["private_kubernetes_api_url"], KUBECONFIG: upstream["kubeconfig"], CONTEXT: upstream["context"], CONFIG_MAP_COUNT: CONFIG_MAP_COUNT, SECRET_COUNT: SECRET_COUNT},
+    {commit: commit, cluster: "upstream", test: "create_load.mjs", ConfigMaps: CONFIG_MAP_COUNT, Secrets: SECRET_COUNT},
+    "k6/create_k8s_resources.js", false
+)
 const testerAddresses = getAppAddressesFor(tester)
 
 // Output access details
