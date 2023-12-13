327b4d984424f7b1a9dd7417ab876b9ababd1877
Silvio Moioli <silvio@moioli.net>
2023-12-13 16:56:14 +0100

    quality of life: use more threads against APIs
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- bin/teardown.mjs
+++ bin/teardown.mjs
@@ -2,4 +2,4 @@
 import {terraformDir, terraformVar, q, run} from "./lib/common.mjs"
 
 run(`terraform -chdir=${q(terraformDir())} init -upgrade`)
-run(`terraform -chdir=${q(terraformDir())} destroy -auto-approve ${q(terraformVar())}`)
+run(`terraform -chdir=${q(terraformDir())} destroy -auto-approve -parallelism=50 ${q(terraformVar())}`)
