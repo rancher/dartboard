8edcbed1e3049d6bb020bc2ff2e57519743193ef
Silvio Moioli <silvio@moioli.net>
2024-06-13 10:10:35 +0200

    scli: add support to importing latest locally built image into k3d
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

---
+++ pkg/docker/docker.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2024 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package docker
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+// Image is the mapping of docker images --format=json
+type Image struct {
+	Repository string
+	Tag        string
+}
+
+// Images returns known docker images matching the image reference
+func Images(image string) ([]string, error) {
+	args := append([]string{"images", "--filter=reference=" + image, "--format=json"})
+	log.Printf("Exec: docker %s\n", strings.Join(args, " "))
+
+	cmd := exec.Command("docker", args...)
+	var outStream strings.Builder
+	var errStream strings.Builder
+	cmd.Stdout = &outStream
+	cmd.Stderr = &errStream
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf(errStream.String())
+	}
+
+	lines := strings.Split(strings.TrimSpace(outStream.String()), "\n")
+	var images []string
+	for _, line := range lines {
+		if line != "" {
+			var img Image
+			err := json.Unmarshal([]byte(line), &img)
+			if err != nil {
+				return nil, fmt.Errorf("error unmarshalling JSON output from docker images: %w", err)
+			}
+			images = append(images, img.Repository+":"+img.Tag)
+		}
+	}
+	return images, nil
+}
---
+++ pkg/k3d/k3d.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package k3d
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/moio/scalability-tests/pkg/terraform"
+)
+
+func ImageImport(cluster terraform.Cluster, image string) error {
+	args := append([]string{"image", "import", "--cluster", strings.Replace(cluster.Context, "k3d-", "", -1), image})
+	log.Printf("Exec: docker %s\n", strings.Join(args, " "))
+
+	cmd := exec.Command("k3d", args...)
+	var errStream strings.Builder
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = &errStream
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf(errStream.String())
+	}
+
+	return nil
+}
