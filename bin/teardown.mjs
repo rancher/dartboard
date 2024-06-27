701e2c9a7201341726f232b63fd68fd379a91d6f
Silvio Moioli <silvio@moioli.net>
2024-06-27 15:52:25 +0200

    refactoring: rename away from terraform
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- bin/teardown.mjs
+++ bin/teardown.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {tofuDir, terraformVar, q, run} from "./lib/common.mjs"
+import {tofuDir, tofuVar, q, run} from "./lib/common.mjs"
 
 run(`tofu -chdir=${q(tofuDir())} init -upgrade`)
-run(`tofu -chdir=${q(tofuDir())} destroy -auto-approve ${q(terraformVar())}`)
+run(`tofu -chdir=${q(tofuDir())} destroy -auto-approve ${q(tofuVar())}`)
