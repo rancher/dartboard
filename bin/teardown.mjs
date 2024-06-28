7f19038a8e04464d73c4fce6a1d1b05eedc21ca5
Silvio Moioli <silvio@moioli.net>
2024-06-28 13:28:44 +0200

    Drop javascript codebase
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- bin/teardown.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import {tofuDir, tofuVar, q, run} from "./lib/common.mjs"
-
-run(`tofu -chdir=${q(tofuDir())} init -upgrade`)
-run(`tofu -chdir=${q(tofuDir())} destroy -auto-approve ${q(tofuVar())}`)
