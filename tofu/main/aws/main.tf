bcd2cd565c435822172ffda00702fab956a41eb3
Silvio Moioli <silvio@moioli.net>
2024-08-09 15:07:25 +0200

    drop RKE1 support
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/main/aws/main.tf
+++ tofu/main/aws/main.tf
@@ -40,35 +40,6 @@ module "k3s_cluster" {
   vpc_security_group_id     = local.k3s_clusters[count.index].public_ip ? module.network.public_security_group_id : module.network.private_security_group_id
 }
 
-module "rke_cluster" {
-  count        = length(local.rke_clusters)
-  source       = "../../modules/aws_rke"
-  project_name = local.project_name
-  name         = local.rke_clusters[count.index].name
-  server_count = local.rke_clusters[count.index].server_count
-  agent_count  = local.rke_clusters[count.index].agent_count
-  agent_labels = local.rke_clusters[count.index].reserve_node_for_monitoring ? [
-    [{ key : "monitoring", value : "true" }]
-  ] : []
-  agent_taints = local.rke_clusters[count.index].reserve_node_for_monitoring ? [
-    [{ key : "monitoring", value : "true", effect : "NoSchedule" }]
-  ] : []
-  distro_version = local.rke_clusters[count.index].distro_version
-
-  sans                      = ["${local.rke_clusters[count.index].name}.local.gd"]
-  local_kubernetes_api_port = var.first_kubernetes_api_port + length(local.k3s_clusters) + count.index
-  tunnel_app_http_port      = var.first_app_http_port + length(local.k3s_clusters) + count.index
-  tunnel_app_https_port     = var.first_app_https_port + length(local.k3s_clusters) + count.index
-  ami                       = local.rke_clusters[count.index].ami
-  instance_type             = local.rke_clusters[count.index].instance_type
-  availability_zone         = var.availability_zone
-  ssh_key_name              = module.network.key_name
-  ssh_private_key_path      = var.ssh_private_key_path
-  ssh_bastion_host          = module.network.bastion_public_name
-  subnet_id                 = local.rke_clusters[count.index].public_ip ? module.network.public_subnet_id : module.network.private_subnet_id
-  vpc_security_group_id     = local.rke_clusters[count.index].public_ip ? module.network.public_security_group_id : module.network.private_security_group_id
-}
-
 module "rke2_cluster" {
   count        = length(local.rke2_clusters)
   source       = "../../modules/aws_rke2"
@@ -85,9 +56,9 @@ module "rke2_cluster" {
   distro_version = local.rke2_clusters[count.index].distro_version
 
   sans                      = ["${local.rke2_clusters[count.index].name}.local.gd"]
-  local_kubernetes_api_port = var.first_kubernetes_api_port + length(local.k3s_clusters) + length(local.rke_clusters) + count.index
-  tunnel_app_http_port      = var.first_app_http_port + length(local.k3s_clusters) + length(local.rke_clusters) + count.index
-  tunnel_app_https_port     = var.first_app_https_port + length(local.k3s_clusters) + length(local.rke_clusters) + count.index
+  local_kubernetes_api_port = var.first_kubernetes_api_port + length(local.k3s_clusters) + count.index
+  tunnel_app_http_port      = var.first_app_http_port + length(local.k3s_clusters) + count.index
+  tunnel_app_https_port     = var.first_app_https_port + length(local.k3s_clusters) + count.index
   ami                       = local.rke2_clusters[count.index].ami
   instance_type             = local.rke2_clusters[count.index].instance_type
   availability_zone         = var.availability_zone
