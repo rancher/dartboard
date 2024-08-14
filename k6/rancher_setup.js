eca0f5b14fc5ab82e8b2a518c711da7e9858944c
Silvio Moioli <silvio@moioli.net>
2024-08-14 13:16:26 +0200

    bugfix: run first login init even in absence of downstream clusters
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- k6/rancher_setup.js
+++ k6/rancher_setup.js
@@ -9,7 +9,7 @@ export default function main() {
     const baseUrl = __ENV.BASE_URL
     const bootstrapPassword = __ENV.BOOTSTRAP_PASSWORD
     const password = __ENV.PASSWORD
-    const importedClusterNames = __ENV.IMPORTED_CLUSTER_NAMES.split(",")
+    const importedClusterNames = __ENV.IMPORTED_CLUSTER_NAMES === "" ? [] : __ENV.IMPORTED_CLUSTER_NAMES.split(",")
 
     const cookies = getCookies(baseUrl)
 
