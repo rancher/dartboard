6ed76f0eb36b2fe39ebb35ff9c7518a9d3c9d90a
Silvio Moioli <silvio@moioli.net>
2024-09-25 14:43:48 +0200

    bump harvester provider
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/modules/harvester_k3s /terraform.tf
+++ tofu/modules/harvester_k3s /terraform.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "1.6.2"
+  required_version = "> 1.6.2"
   required_providers {
     harvester = {
-      source  = "harvester/harvester"
-      version = "0.6.4"
+      source = "harvester/harvester"
     }
   }
 }
--- tofu/modules/harvester_network/terraform.tf
+++ tofu/modules/harvester_network/terraform.tf
@@ -3,7 +3,6 @@ terraform {
   required_providers {
     harvester = {
       source = "harvester/harvester"
-      # version = "0.6.4"
     }
   }
 }
