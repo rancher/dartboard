dfcc0a13c0234acd9e7bb512e21e3fd42c43709f
Silvio Moioli <silvio@moioli.net>
2024-11-06 15:29:03 +0100

    drop unneeded parts of script
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- .github/scripts/build-packages.sh
+++ .github/scripts/build-packages.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -e
 
-# dartboard proper
 make
-
-# other programs in the repo
-oldPWD="$(pwd)"
-
-dirs=("./scripts/soak" "./test")
-
-for dir in "${dirs[@]}"; do
-    echo "Building $dir"
-    cd "$dir"
-    go build ./...
-    cd "$oldPWD"
-done
