2d8774356516513bfbf2504216a4375da7615527
Silvio Moioli <silvio@moioli.net>
2024-12-09 10:06:00 +0100

    Unbreak AWS cluster naming (#30)

--- tofu/main/aws/locals.tf
+++ tofu/main/aws/locals.tf
@@ -1,14 +1,17 @@
 locals {
-  downstream_clusters = flatten([
+  unnamed_downstream_clusters = flatten([
     for i, template in var.downstream_cluster_templates : [
-      for j in range(template.cluster_count) : merge(template, { name = "${template.name_prefix}${i}-${j}" })
+      for j in range(template.cluster_count) : template
   ] if template.cluster_count > 0 ])
-  all_clusters = flatten(concat([var.upstream_cluster],
+
+  downstream_clusters = [for i, cluster in local.unnamed_downstream_clusters : merge(cluster, {name = "downstream-${i}"})]
+
+  all_clusters = flatten(concat(
+    [merge(var.upstream_cluster, {name = "upstream"})],
     local.downstream_clusters,
-    var.deploy_tester_cluster ? [var.tester_cluster] : []
+    var.deploy_tester_cluster ? [[merge(var.tester_cluster, {name = "tester"})]] : []
   ))
 
-
-  k3s_clusters  = [for i, cluster in local.all_clusters : merge(cluster, {name = "${cluster.name_prefix}-${i}"}) if strcontains(cluster.distro_version, "k3s")]
-  rke2_clusters = [for i, cluster in local.all_clusters : merge(cluster, {name = "${cluster.name_prefix}-${i}"}) if strcontains(cluster.distro_version, "rke2")]
+  k3s_clusters  = [for i, cluster in local.all_clusters : cluster if strcontains(cluster.distro_version, "k3s")]
+  rke2_clusters = [for i, cluster in local.all_clusters : cluster if strcontains(cluster.distro_version, "rke2")]
 }
