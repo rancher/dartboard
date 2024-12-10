e572d870bb9d3f884c16b0a0679fab92c8f97b23
Silvio Moioli <silvio@moioli.net>
2024-12-10 13:09:04 +0100

    aws: uniform naming
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/main/aws/main.tf
+++ tofu/main/aws/main.tf
@@ -9,7 +9,7 @@ module "network" {
   region               = var.region
   availability_zone    = var.availability_zone
   bastion_host_ami     = length(var.bastion_host_ami) > 0 ? var.bastion_host_ami : null
-  ssh_user             = var.ssh_user
+  ssh_bastion_user     = var.ssh_user
   ssh_public_key_path  = var.ssh_public_key_path
   ssh_private_key_path = var.ssh_private_key_path
 }
