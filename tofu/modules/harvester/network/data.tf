83c97c95b70ab4ee339bb6ae3c4a61c2541e8a21
Silvio Moioli <silvio@moioli.net>
2025-01-17 08:47:30 +0100

    bugfix: add missing namespace parameter
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/modules/harvester/network/data.tf
+++ tofu/modules/harvester/network/data.tf
@@ -1,4 +1,5 @@
 data "harvester_network" "this" {
-  count = var.network_details.create ? 0 : 1
-  name  = var.network_details.name
+  count     = var.network_details.create ? 0 : 1
+  name      = var.network_details.name
+  namespace = var.network_details.namespace
 }
