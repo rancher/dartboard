b8ee1b571bc7ff63dcab137fdb6619f44a9e0528
Michael Spencer <MSpencer87@users.noreply.github.com>
2025-03-17 09:46:24 -0700

    Usage improvements, added README

--- scripts/utils/import-metrics-mimirtool.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# set kubeconfig
-export KUBECONFIG=$1
-
-# run mimirtool, keep shell open
-screen -t -s -d -m bash -c "kubectl -n cattle-monitoring-system run mimirtool --rm -ti --image=grafana/mimirtool:2.13.0 --env config.expand-env=true --env querier.max-samples=100000000 --command -- sh" &
