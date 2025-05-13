6a2076bd5c34590c31898faa6181652e6bc25658
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2025-05-13 20:05:29 +0000

    replace pod status function from shepherd with custom action

---
+++ internal/actions/pods.go
@@ -0,0 +1,58 @@
+package actions
+
+import (
+	"context"
+	"time"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	shepherdpods "github.com/rancher/shepherd/extensions/workloads/pods"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+const (
+	PodResourceSteveType = "pod"
+)
+
+// StatusPods is a helper function that uses the steve client to list pods on a namespace for a specific cluster
+// and return the statuses in a list of strings
+func StatusPodsWithTimeout(client *rancher.Client, clusterID string, timeout time.Duration) []error {
+	downstreamClient, err := client.Steve.ProxyDownstream(clusterID)
+	if err != nil {
+		return []error{err}
+	}
+
+	var podErrors []error
+
+	steveClient := downstreamClient.SteveType(PodResourceSteveType)
+	// Create a context with timeout
+	ctx := context.Background()
+	err = wait.PollUntilContextTimeout(ctx, 5*time.Second, timeout, true, func(ctx context.Context) (done bool, err error) {
+		// emptying pod errors every time we poll so that we don't return stale errors
+		podErrors = []error{}
+
+		pods, err := steveClient.List(nil)
+		if err != nil {
+			// not returning the error in this case, as it could cause a false positive if we start polling too early.
+			return false, nil
+		}
+
+		for _, pod := range pods.Data {
+			isReady, err := shepherdpods.IsPodReady(&pod)
+			if !isReady {
+				// not returning the error in this case, as it could cause a false positive if we start polling too early.
+				return false, nil
+			}
+
+			if err != nil {
+				podErrors = append(podErrors, err)
+			}
+		}
+		return true, nil
+	})
+
+	if err != nil {
+		podErrors = append(podErrors, err)
+	}
+
+	return podErrors
+}
