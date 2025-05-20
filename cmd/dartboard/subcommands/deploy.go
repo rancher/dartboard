2b76b131779d3f3c5b5a85a280eaa3284670d94b
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2025-05-20 02:19:22 +0000

    cleanup some comments

--- cmd/dartboard/subcommands/deploy.go
+++ cmd/dartboard/subcommands/deploy.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/rancher/dartboard/internal/dart"
 	"github.com/rancher/dartboard/internal/helm"
+	"github.com/rancher/dartboard/internal/kubectl"
 	"github.com/rancher/dartboard/internal/tofu"
 	"github.com/rancher/shepherd/pkg/session"
 	cli "github.com/urfave/cli/v2"
@@ -64,53 +65,53 @@ func Deploy(cli *cli.Context) error {
 		return err
 	}
 
-	// // Helm charts
-	// tester := clusters["tester"]
+	// Helm charts
+	tester := clusters["tester"]
 
-	// if err = chartInstall(tester.Kubeconfig, chart{"k6-files", "tester", "k6-files"}, nil); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstall(tester.Kubeconfig, chart{"mimir", "tester", "mimir"}, nil); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstall(tester.Kubeconfig, chart{"grafana-dashboards", "tester", "grafana-dashboards"}, nil); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstallGrafana(r, &tester); err != nil {
-	// 	return err
-	// }
+	if err = chartInstall(tester.Kubeconfig, chart{"k6-files", "tester", "k6-files"}, nil); err != nil {
+		return err
+	}
+	if err = chartInstall(tester.Kubeconfig, chart{"mimir", "tester", "mimir"}, nil); err != nil {
+		return err
+	}
+	if err = chartInstall(tester.Kubeconfig, chart{"grafana-dashboards", "tester", "grafana-dashboards"}, nil); err != nil {
+		return err
+	}
+	if err = chartInstallGrafana(r, &tester); err != nil {
+		return err
+	}
 
 	upstream := clusters["upstream"]
-	// rancherVersion := r.ChartVariables.RancherVersion
-	// rancherImageTag := "v" + rancherVersion
-	// if r.ChartVariables.RancherImageTagOverride != "" {
-	// 	rancherImageTag = r.ChartVariables.RancherImageTagOverride
-	// 	err = importImageIntoK3d(tf, "rancher/rancher:"+rancherImageTag, upstream)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
+	rancherVersion := r.ChartVariables.RancherVersion
+	rancherImageTag := "v" + rancherVersion
+	if r.ChartVariables.RancherImageTagOverride != "" {
+		rancherImageTag = r.ChartVariables.RancherImageTagOverride
+		err = importImageIntoK3d(tf, "rancher/rancher:"+rancherImageTag, upstream)
+		if err != nil {
+			return err
+		}
+	}
 
-	// if err = chartInstallCertManager(r, &upstream); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstallRancher(r, rancherImageTag, &upstream); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstallRancherIngress(&upstream); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstallCgroupsExporter(&upstream); err != nil {
-	// 	return err
-	// }
+	if err = chartInstallCertManager(r, &upstream); err != nil {
+		return err
+	}
+	if err = chartInstallRancher(r, rancherImageTag, &upstream); err != nil {
+		return err
+	}
+	if err = chartInstallRancherIngress(&upstream); err != nil {
+		return err
+	}
+	if err = chartInstallCgroupsExporter(&upstream); err != nil {
+		return err
+	}
 
-	// // Wait for Rancher deployments to be complete, or subsequent steps may fail
-	// if err = kubectl.WaitRancher(upstream.Kubeconfig); err != nil {
-	// 	return err
-	// }
-	// if err = chartInstallRancherMonitoring(r, &upstream); err != nil {
-	// 	return err
-	// }
+	// Wait for Rancher deployments to be complete, or subsequent steps may fail
+	if err = kubectl.WaitRancher(upstream.Kubeconfig); err != nil {
+		return err
+	}
+	if err = chartInstallRancherMonitoring(r, &upstream); err != nil {
+		return err
+	}
 
 	// Setup rancher client
 	upstreamAdd, err := getAppAddressFor(upstream)
@@ -139,23 +140,6 @@ func Deploy(cli *cli.Context) error {
 	}
 	SortItemsNaturally(downstreamClusters, func(c tofu.Cluster) string { return c.Name })
 
-	// TODELETE:
-	// // Filter out Cluster Templates that are for provisioning
-	// var provisionClusterTemplates []dart.ClusterTemplate
-	// var customClusterTemplates []dart.ClusterTemplate
-
-	// for _, template := range r.ClusterTemplates {
-	// 	if template.ClusterConfig != nil {
-	// 		if !template.IsCustomCluster {
-	// 			provisionClusterTemplates = append(provisionClusterTemplates, template)
-	// 		} else {
-	// 			customClusterTemplates = append(customClusterTemplates, template)
-	// 		}
-	// 	} else {
-	// 		return fmt.Errorf("error, did not find any Provisioning or Custom Cluster Templates")
-	// 	}
-	// }
-
 	jsonBytes, err := json.MarshalIndent(downstreamClusters, "", "    ")
 	if err != nil {
 		fmt.Println("Error marshaling JSON:", err)
@@ -176,13 +160,6 @@ func Deploy(cli *cli.Context) error {
 			return err
 		}
 	}
-
-	// TODELETE:
-	// err = handleCustomClusters(cli, r, custom_clusters, rancherClient, &rancherConfig)
-	// if err != nil {
-	// 	return err
-	// }
-	// }
 
 	if len(r.ClusterTemplates) > 0 {
 		// If we have provision Cluster Templates then setup Harvester Client + import Harvester Cluster into Rancher
