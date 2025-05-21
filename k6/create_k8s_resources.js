4b901d3a20589803db680827c4a6bdd46e544f3b
Silvio Moioli <silvio@moioli.net>
2025-05-21 07:12:52 +0100

    create_k8s_resources: make vus parametric
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- k6/create_k8s_resources.js
+++ k6/create_k8s_resources.js
@@ -7,9 +7,9 @@ import * as k8s from './k8s.js'
 const namespace = __ENV.NAMESPACE || "scalability-test"
 const configMapCount = Number(__ENV.CONFIG_MAP_COUNT)
 const secretCount = Number(__ENV.SECRET_COUNT)
-const vus = 1
 const dataSize = Number(__ENV.DATA_SIZE || 10*1024)
 const data = encoding.b64encode("a".repeat(dataSize))
+const vus = Number(__ENV.VUS || 11)
 
 // Option setting
 const kubeconfig = k8s.kubeconfig(__ENV.KUBECONFIG, __ENV.CONTEXT)
