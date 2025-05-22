0d9feb7f232351beebc6e64470fa0a7693a430fb
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2025-05-22 01:40:32 +0000

    update upstream and tester clusters with new create_tunnels input

--- tofu/modules/generic/test_environment/variables.tf
+++ tofu/modules/generic/test_environment/variables.tf
@@ -41,6 +41,7 @@ variable "upstream_cluster" {
     public_ip                   = bool // Whether the upstream cluster should have a public IP assigned
     reserve_node_for_monitoring = bool // Set a 'monitoring' label and taint on one node of the upstream cluster to reserve it for monitoring
     enable_audit_log            = bool // Enable audit log for the cluster
+    create_tunnels              = bool // Whether ssh tunnels to the downstream cluster's first server node should be created. Default false
 
     node_module_variables = any // Node module-specific variables
   })
@@ -123,6 +124,7 @@ variable "tester_cluster" {
     public_ip                   = bool // Whether the tester cluster should have a public IP assigned
     reserve_node_for_monitoring = bool // Set a 'monitoring' label and taint on one node of the tester cluster to reserve it for monitoring
     enable_audit_log            = bool // Enable audit log for the cluster
+    create_tunnels              = bool // Whether ssh tunnels to the downstream cluster's first server node should be created. Default false
 
     node_module_variables = any // Node module-specific variables
   })                            # If null, no tester cluster will be created
