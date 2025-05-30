0eeff27aea012ef6cb08467c1784d1ec3c6525c0
Silvio Moioli <silvio@moioli.net>
2025-05-30 19:18:37 +0200

    tofu/aws: bugfix: correct vpc creation logic (#66)
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/modules/aws/network/outputs.tf
+++ tofu/modules/aws/network/outputs.tf
@@ -3,7 +3,7 @@ output "config" {
     availability_zone : var.availability_zone,
     public_subnet_id : local.public_subnet_id,
     private_subnet_id : local.private_subnet_id,
-    secondary_private_subnet_id : var.secondary_availability_zone != null ? local.secondary_private_subnet_id : null,
+    secondary_private_subnet_id : local.secondary_private_subnet_id,
     public_security_group_id : aws_security_group.public.id,
     private_security_group_id : aws_security_group.private.id,
     ssh_key_name : aws_key_pair.key_pair.key_name,
