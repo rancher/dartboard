d6c98211022ec811c69fe14a336583eeaf5ba082
Silvio Moioli <silvio@moioli.net>
2025-06-06 14:37:59 +0200

    bugfix: allow for a larger bastion to accommodate larger setups
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/modules/aws/network/variables.tf
+++ tofu/modules/aws/network/variables.tf
@@ -43,7 +43,7 @@ variable "bastion_host_ami" {
 
 variable "bastion_host_instance_type" {
   description = "EC2 instance type"
-  default     = "t4g.small"
+  default     = "t4g.large"
 }
 
 # Variables for existing VPC configuration
