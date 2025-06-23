f33c4cbf20b3f3aefd887a977ec6743d1d566719
Michael Spencer <MSpencer87@users.noreply.github.com>
2025-06-23 00:53:30 -0700

    initial commit, updates for independent count values

---
+++ k6/generic/generic_utils.js
@@ -0,0 +1,113 @@
+import {check, sleep} from 'k6';
+import http from 'k6/http';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+// Create Config Maps
+// Required params: iter, name, namespace, baseurl, data, cookies
+export function createConfigMaps(iter, namespace, baseUrl, data, cookies) {
+    const name = `test-config-map-${iter}`
+
+    const res = http.post(`${baseUrl}/api/v1/namespaces/${namespace}/configmaps`,
+        JSON.stringify({
+            "metadata": {
+                "name": name,
+                "namespace": namespace
+            },
+            "data": {"data": data}
+        }),
+        { cookies: cookies }
+    )
+
+    sleep(0.1)
+    if (res.status != 201) {
+        console.log(res)
+    }
+    check(res, {
+        '/v1/configmaps returns status 201': (r) => r.status === 201,
+    })
+}
+
+
+// Create Secrets
+// Required params: iter, name, namespace, baseurl, data
+export function createSecrets(iter, namespace, baseUrl, data, cookies) {
+    const name = `test-secrets-${iter}`
+
+    const res = http.post(`${baseUrl}/api/v1/namespaces/${namespace}/secrets`,
+        JSON.stringify({
+            "metadata": {
+                "name": name,
+                "namespace": namespace
+            },
+            "data": {"data": data}
+        }),
+        { cookies: cookies }
+    )
+
+    sleep(0.1)
+    if (res.status != 201) {
+        console.log(res)
+    }
+    check(res, {
+        '/v1/secrets returns status 201': (r) => r.status === 201,
+    })
+}
+
+
+// Create Deployments 
+// Required params: iter, name, namespace, baseurl, cookies
+export function createDeployments(iter, namespace, baseUrl, cookies) {
+    const name = `test-deployment-${iter}`
+
+    const res = http.post(`${baseUrl}/v1/apps.deployments`,
+        JSON.stringify({
+            "apiVersion": "apps/v1",
+            "kind": "Deployment",
+            "metadata": {
+              "name": name,
+              "namespace": namespace
+            },
+            "spec": {
+              "selector": {
+                "matchLabels": {
+                  "app": name
+                }
+              },
+              "template": {
+                "metadata": {
+                  "labels": {
+                    "app": name
+                  }
+                },
+                "spec": {
+                  "containers": [
+                    {
+                      "command": [
+                        "bash",
+                        "-c",
+                        `echo test; sleep ${randomIntBetween(10,60)}; exit 1;`
+                      ],
+                      "image": "ubuntu",
+                      "name": name
+                    }
+                  ],
+                  "securityContext": {
+                    "runAsUser": 2000,
+                    "runAsGroup": 3000
+                  }
+                }
+              }
+            }
+        }),
+        { cookies: cookies }
+    )
+
+    sleep(0.1)
+    if (res.status != 201) {
+        console.log(res)
+    }
+    check(res, {
+        '/v1/apps.deployments returns status 201': (r) => r.status === 201,
+    })
+
+}
