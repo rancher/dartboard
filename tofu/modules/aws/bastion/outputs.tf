df9e43320b52977faead4a808545410d19170522
Mario Manno <mario.manno@suse.com>
2025-09-16 10:57:25 +0200

    fixup! Split AWS bastion host into a separate module

--- tofu/modules/aws/bastion/outputs.tf
+++ tofu/modules/aws/bastion/outputs.tf
@@ -15,3 +15,15 @@ output "public_name" {
   depends_on = [null_resource.host_configuration]
   value      = aws_instance.instance.public_dns
 }
+
+output "public_ip" {
+  value = aws_instance.instance.public_ip
+}
+
+output "ssh_user" {
+  value = var.ssh_user
+}
+
+output "ssh_key_path" {
+  value = var.ssh_private_key_path
+}
