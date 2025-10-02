083d2376aeca6748b3921d1b9dfdc291345a9d96
Michael Spencer <MSpencer87@users.noreply.github.com>
2025-10-02 11:37:34 -0700

    updated params

--- k6/generic/generic_utils.js
+++ k6/generic/generic_utils.js
@@ -1,15 +1,14 @@
 import {check, sleep} from 'k6';
 import http from 'k6/http';
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 // Create Config Maps
-// Required params: iter, name, namespace, baseurl, data, cookies
-export function createConfigMaps(baseUrl, cookies, cluster, namespace, data, iter) {
+// Required params: baseurl, coookies, data, cluster, namespace, iter
+export function createConfigMaps(baseUrl, cookies, data, clusterId, namespace, iter) {
     const name = `test-config-map-${iter}`
 
-    const url = cluster === "local"?
+    const url = clusterId === "local"?
       `${baseUrl}/v1/configmaps` :
-      `${baseUrl}/k8s/clusters/${cluster}/v1/configmaps`
+      `${baseUrl}/k8s/clusters/${clusterId}/v1/configmaps`
 
     const res = http.post(`${url}`,
         JSON.stringify({
@@ -33,13 +32,13 @@ export function createConfigMaps(baseUrl, cookies, cluster, namespace, data, ite
 
 
 // Create Secrets
-// Required params: iter, name, namespace, baseurl, data
-export function createSecrets(baseUrl, cookies, cluster, namespace, data, iter)  {
+// Required params: baseurl, coookies, data, cluster, namespace, iter
+export function createSecrets(baseUrl, cookies, data, clusterId, namespace, iter)  {
     const name = `test-secrets-${iter}`
 
-    const url = cluster === "local"?
+    const url = clusterId === "local"?
       `${baseUrl}/v1/secrets` :
-      `${baseUrl}/k8s/clusters/${cluster}/v1/secrets`
+      `${baseUrl}/k8s/clusters/${clusterId}/v1/secrets`
 
     const res = http.post(`${url}`,
         JSON.stringify({
@@ -64,13 +63,13 @@ export function createSecrets(baseUrl, cookies, cluster, namespace, data, iter) 
 
 
 // Create Deployments 
-// Required params: iter, name, namespace, baseurl, cookies
-export function createDeployments(baseUrl, cookies, cluster, namespace, iter) {
+// Required params: baseurl, coookies, cluster, namespace, iter
+export function createDeployments(baseUrl, cookies, clusterId, namespace, iter) {
     const name = `test-deployment-${iter}`
 
-    const url = cluster === "local"?
+    const url = clusterId === "local"?
       `${baseUrl}/v1/apps.deployments` :
-      `${baseUrl}/k8s/clusters/${cluster}/v1/apps.deployments`
+      `${baseUrl}/k8s/clusters/${clusterId}/v1/apps.deployments`
 
 
     const res = http.post(`${url}`,
