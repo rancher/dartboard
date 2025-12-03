17306d40896b44d99ff96e094e186caf3442b7b1
Silvio Moioli <silvio@moioli.net>
2025-12-03 09:44:12 +0100

    k3d: bump docker provider
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/main/k3d/terraform.tf
+++ tofu/main/k3d/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.23.1"
+      version = "3.9.0"
     }
     k3d = {
       source  = "moio/k3d"
