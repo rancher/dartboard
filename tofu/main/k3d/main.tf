1b500d8676a74290951a067e2cf6bf82d3901a7a
Silvio Moioli <silvio@moioli.net>
2025-12-03 09:46:53 +0100

    k3d: unbreak, adding support for features introduced in other providers
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- tofu/main/k3d/main.tf
+++ tofu/main/k3d/main.tf
@@ -17,7 +17,7 @@ module "test_environment" {
   downstream_cluster_distro_module = var.downstream_cluster_distro_module
   tester_cluster                   = var.tester_cluster
   tester_cluster_distro_module     = var.tester_cluster_distro_module
-  node_module                      = null
+  node_module                      = "k3d/node"
   ssh_user                         = null
   ssh_private_key_path             = null
   network_config                   = module.network.config
