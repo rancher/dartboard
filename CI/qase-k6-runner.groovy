9a2eb7e4dcc950b5dc70ca4d7499d054ad8322c0
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2025-12-19 19:18:32 -0500

    fix k6 env parsing

--- CI/qase-k6-runner.groovy
+++ CI/qase-k6-runner.groovy
@@ -10,7 +10,7 @@ if (params.JENKINS_AGENT_LABEL) {
 def kubeconfigContainerPath
 def baseURL
 def sanitizeCharacterRegex = "[^a-zA-Z0-9'_-]"
-def sanitizeK6EnvRegex = "[^a-zA-Z0-9_=,;&*-\n\r]"
+def sanitizeK6EnvRegex = "[^a-zA-Z0-9_=,;&*-.]"
 
 pipeline {
   agent { label agentLabel }
