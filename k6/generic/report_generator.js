88ea13604a0c921a3825ce78f1a9b82810abdf95
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2025-12-22 11:58:07 -0500

    introduce hacky workaround to generate both the native web dashboard report + custom reports

---
+++ k6/generic/report_generator.js
@@ -0,0 +1,23 @@
+import { customHandleSummary } from './k6_utils.js';
+
+const summaryPath = __ENV.K6_SUMMARY_JSON_FILE || "/home/ivln/workspace/work/RancherVCS/dartboard/k6/generic/summary.json";
+
+// Load summary data during initialization
+let data = null;
+if (summaryPath) {
+    try {
+        data = JSON.parse(open(summaryPath));
+    } catch (e) {
+        console.error(`Failed to parse summary JSON from ${summaryPath}: ${e.message}`);
+    }
+}
+
+export function handleSummary() {
+    if (!data) {
+        console.log("No summary data available to generate reports.");
+        return {};
+    }
+    return customHandleSummary(data);
+}
+
+export default function() {}
