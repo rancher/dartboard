d1883176f817e5975f68cb21a6b30e03c4ae5efa
Iramis Valentin <iramis.valentin@suse.com>
2026-05-11 12:01:01 -0400

    Update tofu modules for custom cluster support (#178)
    * update tofu modules for custom cluster support
    
    - Add custom_clusters output to AWS and Harvester main modules
    - Add ssh tunnel enable/disable flag to node modules
    - Update test_environment for node-only creation (custom clusters)
    - Update image datasource logic to skip if image_id is provided
    - Remove unused node_templates variable from harvester
    - Minor formatting and cleanup in rke2/k3s modules
    
    * add required field to darts
    
    * remove redundant ternary
    
    * update provider versions to latest stable and sync tofu/terraform required version for now
    
    * remove restriction on # of etcd nodes

--- tofu/main/aws/terraform.tf
+++ tofu/main/aws/terraform.tf
@@ -1,16 +1,13 @@
 terraform {
+  required_version = ">=1.8.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.9.0"
+      version = "6.42.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "4.1.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.0.2"
+      version = "4.2.1"
     }
     ssh = {
       source  = "loafoe/ssh"
--- tofu/modules/harvester/node/outputs.tf
+++ tofu/modules/harvester/node/outputs.tf
@@ -14,10 +14,6 @@ output "private_ip" {
   value = var.network_config.public ? local.public_network_interfaces[0].ip_address : local.private_network_interfaces[0].ip_address
 }
 
-output "public_address" {
-  value = var.network_config.public ? local.public_network_interfaces[0].ip_address : local.private_network_interfaces[0].ip_address
-}
-
 output "public_name" {
   value = "${var.network_config.public ? local.public_network_interfaces[0].ip_address : local.private_network_interfaces[0].ip_address}.sslip.io"
 }
