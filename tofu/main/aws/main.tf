b26c5a5b8dd6f2fdec8bec99cd4c09285d2dc88d
Michael Spencer <MSpencer87@users.noreply.github.com>
2026-05-19 11:20:05 -0700

    updated formatting

--- tofu/main/aws/main.tf
+++ tofu/main/aws/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region  = var.region
-  profile = var.aws_profile
+  region     = var.region
+  profile    = var.aws_profile
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
